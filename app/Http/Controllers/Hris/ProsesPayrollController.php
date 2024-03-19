d3e21f1f66570ac673b4716eba0cacbd54b977d4
Rifqi <rdzulfikri@gmail.com>
2024-03-19 08:41:05 +0700

    tes2

--- app/Http/Controllers/Hris/ProsesPayrollController.php
+++ app/Http/Controllers/Hris/ProsesPayrollController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 
+use App\Test;
 use App\Models\MasterDataAbsenKehadiran;
 use App\Models\RefAbsenIjin;
 use App\Models\DataKoreksiPotongan;
