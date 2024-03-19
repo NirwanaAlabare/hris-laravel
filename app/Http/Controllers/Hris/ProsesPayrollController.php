52864145986627d16317b340a877d6ba2933e37b
it-nag <ramon@ptnag.com>
2024-03-19 08:40:03 +0700

    dafda

--- app/Http/Controllers/Hris/ProsesPayrollController.php
+++ app/Http/Controllers/Hris/ProsesPayrollController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 
+use App\Models\Notifications;
 use App\Models\MasterDataAbsenKehadiran;
 use App\Models\RefAbsenIjin;
 use App\Models\DataKoreksiPotongan;
@@ -2593,7 +2594,6 @@ class ProsesPayrollController extends AdminBaseController
                         'upah_per_menit'=>$value->rekap_kehadiran[0]->gaji_menit,
                         'potongan_kehadiran_rupiah'=>$value->rekap_kehadiran[0]->potongan_kehadiran_rupiah,
                         'kehadiran_m_estimasi'=>$value->rekap_kehadiran[0]->kehadiran_m_estimasi,
-                        //esoifosidojgoag
                         'lembur_1'=>$value->rekap_lembur->sum('lembur_1'),
                         'lembur_2'=>$value->rekap_lembur->sum('lembur_2'),
                         'lembur_3'=>$value->rekap_lembur->sum('lembur_3'),
