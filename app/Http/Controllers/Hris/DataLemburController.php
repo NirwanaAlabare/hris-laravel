a3fb46cfff895017711b52f44f3f3a7a72a3efcf
Rifqi <rdzulfikri@gmail.com>
2024-03-25 10:04:50 +0700

    datalemburcontroller edit 1

--- app/Http/Controllers/Hris/DataLemburController.php
+++ app/Http/Controllers/Hris/DataLemburController.php
@@ -2198,7 +2198,9 @@ class DataLemburController extends AdminBaseController
             array_push($jumlah_lembur,$hour_lembur);
             array_push($jumlah_jam_istirahat,$hour_istirahat);
             array_push($keterangan_lembur,$data[0][$i][5]);
-            array_push($status_absen,$actual->status_absen);
+            if(isset($actual->status_absen)){
+                array_push($status_absen,$actual->status_absen);
+            }
         }
         $arrayEmployee=[];
         foreach($enroll_id as $key=>$value){
