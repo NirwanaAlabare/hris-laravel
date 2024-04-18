205ca39aa17c0b1ce20df8019f189705ef977118
Rifqi <rdzulfikri@gmail.com>
2024-04-18 11:37:45 +0700

    rincian kehadiran karyawan fixed

--- resources/views/hris/Laporan/rincian_kehadiran_karyawan.blade.php
+++ resources/views/hris/Laporan/rincian_kehadiran_karyawan.blade.php
@@ -1,105 +1,120 @@
-<html>
-	<head>
-	</head>
-	<body>
-		@foreach ($employee as $value)
-		<table>
-			<tr>
-				<td>PT. NIRWANA ALABARE GARMENT</td>
-			</tr>
-			<tr>
-				<td>Laporan Rinci Kehadiran Karyawan</td>
-			</tr>
-			<tr>
-				<td style="height: 10px"></td>
-			</tr>
-			<tr>
-				<td width="520" style="border-bottom: 1px solid black;">{{$tanggal_awal_absen}} - {{$tanggal_akhir_absen}}</td>
-			</tr>
-		</table>
-		<table class="textkecil">
-			<tr>
-				<td width="90">NIP</td>
-				<td width="160">{{$value->nik}}</td>
-				<td width="90">JABATAN</td>
-				<td>{{$value->status_staff}}</td>
-			</tr>
-			<tr>
-				<td>NAMA KARYAWAN</td>
-				<td>{{$value->employee_name}}</td>
-				<td>BAGIAN</td>
-				<td>{{$value->sub_dept_name}}</td>
-			</tr>
-		</table>
-		<table border="1" class="textkecil">
-			<tr>
-				<td>Tanggal</td>
-				<td>Hari</td>
-				<td>Keterangan</td>
-				<td>Jam masuk</td>
-				<td>Jam keluar</td>
-				<td>DT</td>
-				<td>PC</td>
-				<td>Lembur 1</td>
-				<td>Lembur 2</td>
-				<td>Lembur 3</td>
-				<td>Lembur 4</td>
-				<td>Total jam lembur</td>
-			</tr>
-			@foreach ($value->absensi as $val)
-			<?php 
-			$absen_masuk=substr($val->absen_masuk_kerja,0,5);
-			$absen_pulang=substr($val->absen_pulang_kerja,0,5);
-			$jumlah_menit_absen_dt='';
-			if($val->jumlah_menit_absen_dt!=0){
-				$jumlah_menit_absen_dt=$val->jumlah_menit_absen_dt;
-			}
-			$jumlah_menit_absen_pc='';
-			if($val->jumlah_menit_absen_pc!=0){
-				$jumlah_menit_absen_pc=$val->jumlah_menit_absen_pc;
-			}
-			$lembur1='';
-			if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_1')[0])){
-				$lembur1=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_1')[0];
-			}
-			$lembur2='';
-			if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_2')[0])){
-				$lembur2=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_2')[0];
-			}
-			$lembur3='';
-			if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_3')[0])){
-				$lembur3=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_3')[0];
-			}
-			$lembur4='';
-			if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_4')[0])){
-				$lembur4=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_4')[0];
-			}
-			$total_lembur_1234='';
-			if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('total_lembur_1234')[0])){
-				$total_lembur_1234=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('total_lembur_1234')[0];
-			}
-			?>
-			<tr>
-				<td>{{$val->tanggal_berjalan}}</td>
-				<td>{{$val->nama_hari}}</td>
-				<td>{{$val->status_absen}}</td>
-				<td>{{$absen_masuk}}</td>
-				<td>{{$absen_pulang}}</td>
-				<td>{{$jumlah_menit_absen_dt}}</td>
-				<td>{{$jumlah_menit_absen_pc}}</td>
-				<td>{{$lembur1}}</td>
-				<td>{{$lembur2}}</td>
-				<td>{{$lembur3}}</td>
-				<td>{{$lembur4}}</td>
-				<td>{{$total_lembur_1234}}</td>
-			</tr>
-			@endforeach
-		</table>
+
+@foreach ($employee as $value)
+<div class="wrapper-page">
+	<table>
+		<tr>
+			<td style="font-weight: bold">PT. NIRWANA ALABARE GARMENT</td>
+		</tr>
+		<tr>
+			<td style="font-size: 10pt">Laporan Rinci Kehadiran Karyawan</td>
+		</tr>
+		<tr>
+			<td style="height: 10px"></td>
+		</tr>
+		<tr>
+			<td width="520" style="border-bottom: 1px solid black;font-size: 10pt">{{$tanggal_awal_absen}} - {{$tanggal_akhir_absen}}</td>
+		</tr>
+	</table>
+	<table class="textkecil">
+		<tr>
+			<td style="height: 10px"></td>
+		</tr>
+		<tr>
+			<td width="90">NIP</td>
+			<td width="160">: {{$value->nik}}</td>
+			<td width="90">JABATAN</td>
+			<td>: {{$value->status_staff}}</td>
+		</tr>
+		<tr>
+			<td>NAMA KARYAWAN</td>
+			<td>: {{$value->employee_name}}</td>
+			<td>BAGIAN</td>
+			<td>: {{$value->sub_dept_name}}</td>
+		</tr>
+		<tr>
+			<td style="height: 10px"></td>
+		</tr>
+	</table>
+	<table border="1" class="textkecilbanget">
+		<tr>
+			<td width="46">&nbsp;Tanggal</td>
+			<td width="34">&nbsp;Hari</td>
+			<td width="68">&nbsp;Keterangan</td>
+			<td width="45">&nbsp;Jam masuk</td>
+			<td width="45">&nbsp;Jam keluar</td>
+			<td width="17">&nbsp;DT</td>
+			<td width="17">&nbsp;PC</td>
+			<td width="38">&nbsp;Lembur 1</td>
+			<td width="38">&nbsp;Lembur 2</td>
+			<td width="38">&nbsp;Lembur 3</td>
+			<td width="38">&nbsp;Lembur 4</td>
+			<td width="70">&nbsp;Total jam lembur</td>
+		</tr>
+		@foreach ($value->absensi as $val)
+		<?php 
+		$absen_masuk=substr($val->absen_masuk_kerja,0,5);
+		$absen_pulang=substr($val->absen_pulang_kerja,0,5);
+		$jumlah_menit_absen_dt='';
+		if($val->jumlah_menit_absen_dt!=0){
+			$jumlah_menit_absen_dt=$val->jumlah_menit_absen_dt;
+		}
+		$jumlah_menit_absen_pc='';
+		if($val->jumlah_menit_absen_pc!=0){
+			$jumlah_menit_absen_pc=$val->jumlah_menit_absen_pc;
+		}
+		$lembur1='';
+		if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_1')[0])){
+			$lembur1=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_1')[0];
+		}
+		$lembur2='';
+		if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_2')[0])){
+			$lembur2=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_2')[0];
+		}
+		$lembur3='';
+		if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_3')[0])){
+			$lembur3=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_3')[0];
+		}
+		$lembur4='';
+		if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_4')[0])){
+			$lembur4=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_4')[0];
+		}
+		$total_lembur_1234='';
+		if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('total_lembur_1234')[0])){
+			$total_lembur_1234=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('total_lembur_1234')[0];
+		}
+		?>
+		<tr>
+			<td>&nbsp;{{$val->tanggal_berjalan}}</td>
+			<td>&nbsp;{{$val->nama_hari}}</td>
+			<td>&nbsp;{{$val->status_absen}}</td>
+			<td>&nbsp;{{$absen_masuk}}</td>
+			<td>&nbsp;{{$absen_pulang}}</td>
+			<td>&nbsp;{{$jumlah_menit_absen_dt}}</td>
+			<td>&nbsp;{{$jumlah_menit_absen_pc}}</td>
+			<td>&nbsp;{{$lembur1}}</td>
+			<td>&nbsp;{{$lembur2}}</td>
+			<td>&nbsp;{{$lembur3}}</td>
+			<td>&nbsp;{{$lembur4}}</td>
+			<td>&nbsp;{{$total_lembur_1234}}</td>
+		</tr>
 		@endforeach
-	</body>
-</html>
+	</table>
+</div>
+@endforeach
 <style>
 	.textkecil{
 		font-size: 9pt;
+		font-weight: bold;
+	}
+	.textkecilbanget{
+		font-size: 9pt;
+      	border-collapse: collapse;
+	}
+	.wrapper-page {
+		page-break-after: always;
+	}
+
+	.wrapper-page:last-child {
+		page-break-after: avoid;
 	}
 </style>
