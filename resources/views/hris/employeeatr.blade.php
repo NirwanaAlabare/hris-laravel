8c8648bbe353754e04ce189eadb9c4e8b1d6ccd9
Rifqi <rdzulfikri@gmail.com>
2024-05-15 11:26:33 +0700

    employee atr choose employee button icon

--- resources/views/hris/employeeatr.blade.php
+++ resources/views/hris/employeeatr.blade.php
@@ -356,7 +356,7 @@
                         </div>
                         <div class="col-6">
                             {{-- <a href="#" id="btndownloadiddept" class="py-1" style="background-color: #f23535;color:white;padding-left:10px;padding-right:10px; border-radius:3px" data-toggle="tooltip" title="" data-placement="bottom" data-original-title="Download Id Card"><i class="fa fa-download mr-1"></i>ID Card</a> --}}
-                            <a href="#" id="btnselectemployee" class="py-1" style="background-color: #f23535;color:white;padding-left:10px;padding-right:10px; border-radius:3px" data-target="#select_employee" data-toggle="modal" title="" data-placement="bottom" data-original-title="Download Id Card"><i class="fa fa-download mr-1"></i>ID Card</a>
+                            <a href="#" id="btnselectemployee" class="py-1" style="background-color: #15b22d;color:white;padding-left:10px;padding-right:10px; border-radius:3px" data-target="#select_employee" data-toggle="modal" title="" data-placement="bottom" data-original-title="Download Id Card"><i class="fa fa-check-circle mr-1"></i>Pilih Karyawan</a>
                         </div>
                     </div>
                         <table id="datatable-ajax-crud" class="table table-sm table-striped table-hover table-bordered w-100">
