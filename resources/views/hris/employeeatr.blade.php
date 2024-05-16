6fbbfb0776ad0f6b2fb5e51deb528abbab40f7b7
Rifqi <rdzulfikri@gmail.com>
2024-05-16 15:58:51 +0700

    selected count

--- resources/views/hris/employeeatr.blade.php
+++ resources/views/hris/employeeatr.blade.php
@@ -356,9 +356,6 @@
                         </div>
                         <div class="col-6">
                             {{-- <a href="#" id="btndownloadiddept" class="py-1" style="background-color: #f23535;color:white;padding-left:10px;padding-right:10px; border-radius:3px" data-toggle="tooltip" title="" data-placement="bottom" data-original-title="Download Id Card"><i class="fa fa-download mr-1"></i>ID Card</a> --}}
-                            <p class="my-3">
-                                Selected : <span id="checked-employee-count" class="fw-bold">0</span>
-                            </p>
                             <a href="#" id="btnselectemployee" class="py-1" style="background-color: #15b22d;color:white;padding-left:10px;padding-right:10px; border-radius:3px" data-target="#select_employee" data-toggle="modal" title="" data-placement="bottom" data-original-title="Download Id Card"><i class="fa fa-check-circle mr-1"></i>Pilih Karyawan</a>
                             {{-- <a href="#" id="btnselectemployee" class="py-1" style="background-color: #15b22d;color:white;padding-left:10px;padding-right:10px; border-radius:3px" data-target="#select_employee" data-toggle="modal" title="" data-placement="bottom" data-original-title="Download Id Card" onclick="actionCheckedEmployee()"><i class="fa fa-check-circle mr-1"></i>Pilih Karyawan</a> --}}
                         </div>
@@ -377,6 +374,9 @@
                             <tbody>
                             </tbody>
                         </table>
+                        <p class="my-3">
+                            Selected : <span id="checked-employee-count" class="fw-bold">0</span>
+                        </p>
                 </div>
                 <div class="card-footer bg-primary br-br-7 br-bl-7">
                     <div class="text-white"></div>
