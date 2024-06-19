d6b9fa7950a9e153d28b9cc6786333b217a7e9cf
Rifqi <rdzulfikri@gmail.com>
2024-06-19 08:32:22 +0700

    add jam out min to update lintas hari auto

--- app/Console/Commands/UpdateAbsenLintasHari.php
+++ app/Console/Commands/UpdateAbsenLintasHari.php
@@ -117,7 +117,7 @@ class UpdateAbsenLintasHari extends Command
                         $total_PC=0;
                         if(isset(MasterDataAbsenKehadiran::where('enroll_id',$value->enroll_id)->where('tanggal_berjalan',$tanggal_kemarin)->whereColumn('akhir_jam_kerja','<','mulai_jam_kerja')->pluck('akhir_jam_kerja')[0])){
                             $jadwal_pulang_kemarin=MasterDataAbsenKehadiran::where('enroll_id',$value->enroll_id)->where('tanggal_berjalan',$tanggal_kemarin)->whereColumn('akhir_jam_kerja','<','mulai_jam_kerja')->pluck('akhir_jam_kerja')[0];
-                            $jadwal_out_min_kemarin=date("H:i", strtotime('-5 hours', strtotime($jadwal_pulang_kemarin)));
+                            $jadwal_out_min_kemarin=date("H:i", strtotime('-2 hours', strtotime($jadwal_pulang_kemarin)));
                             $jadwal_out_max_kemarin=date("H:i", strtotime('+5 hours 59 minutes', strtotime($jadwal_pulang_kemarin)));
                             $absen_pulang_kemarin=collect($records)->where('tanggal_absen',$value->tanggal_berjalan)->where('enroll_id',$value->enroll_id)->where('absen_log','>=', $jadwal_out_min_kemarin)->where('absen_log','<=', $jadwal_out_max_kemarin)->min('absen_log');
                             $PC = date_diff(date_create($jadwal_pulang_kemarin),date_create($absen_pulang_kemarin));
