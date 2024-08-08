052f38d4fd4ccbb5682e7fb993c64efffc4a1ffe
Rifqi <rdzulfikri@gmail.com>
2024-08-08 14:49:13 +0700

    DataAbsenPerijinanController create and update

--- app/Http/Controllers/Hris/DataAbsenPerijinanController.php
+++ app/Http/Controllers/Hris/DataAbsenPerijinanController.php
@@ -712,7 +712,10 @@ class DataAbsenPerijinanController extends AdminBaseController
                 ->where(function ($query3) {
                     $query3->whereNotIn('kode_hari', [6, 5])
                         ->orWhereNotNull('mulai_jam_kerja');
-                })->where('status_absen','!=','LN')->update([
+                })->where(function ($query4) {
+                    $query4->whereNotIn('status_absen', ['LN'])
+                        ->orWhereNull('status_absen');
+                })->update([
                     'nomor_absen_ijin' => $nomor_form_perizinan,
                     'status_absen' => $kode_absen_ijin,
                     'absen_alasan' => $absen_alasan,
