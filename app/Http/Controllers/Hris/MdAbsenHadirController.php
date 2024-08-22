89460cd18afaf4e64398c286e755b43ba28f3609
Rifqi <rdzulfikri@gmail.com>
2024-08-22 13:06:15 +0700

    testi

--- app/Http/Controllers/Hris/MdAbsenHadirController.php
+++ app/Http/Controllers/Hris/MdAbsenHadirController.php
@@ -3237,7 +3237,10 @@ class MdAbsenHadirController extends AdminBaseController
                             if( $absenIn!=null && $absenOut!=null && ($jadwal_in==null || $jadwal_in!=null)){
                                 $status_absen=null;
                             }
-                            else if(($absenIn==null || $absenOut==null) && ($jadwal_in==null || $jadwal_in!=null)){
+                            else if($absenIn!=null && $absenOut==null && ($jadwal_in==null || $jadwal_in!=null)){
+                                $status_absen='TL';
+                            }
+                            else if($absenIn==null && $absenOut!=null && ($jadwal_in==null || $jadwal_in!=null)){
                                 $status_absen='TL';
                             }
                             else if( $absenIn==null && $absenOut==null && $jadwal_in!=null){
