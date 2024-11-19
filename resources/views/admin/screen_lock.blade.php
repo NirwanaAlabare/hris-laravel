f7cdf03bb4f5fd6d33f798dd76562c6f1af584a3
rifqird <rdzulfikri@gmail.com>
2024-11-19 13:14:50 +0700

    jigoajgoajgoi

--- resources/views/admin/screen_lock.blade.php
+++ resources/views/admin/screen_lock.blade.php
@@ -15,28 +15,30 @@
 						<div class="col-lg-4 d-block mx-auto">
 							<div class="row">
 								<div class="col-xl-12 col-md-12 col-md-12">
-									<div class="card">
-										<div class="card-body">
-											<div class="text-center mb-6">
-												<img src="{{URL::asset('assets/images/brand/logo2.png')}}" class="" alt="">
-											</div>
-											<div class="text-center mb-4 ">
-												<img src="{{URL::asset('assets/images/users/avatars/avatar4.png')}}" alt="user-img" class="avatar avatar-xl brround mCS_img_loaded">
-											</div>
-											<span class="m-4 d-none d-lg-block text-center">												
-												<span class="h4"><strong>{{ $name }}</strong></span>
-												<p class="text-muted"><i class="fa fa-lock"></i> Lock Screen</p>
-											</span>
-											<div class="form-group">
-												<input type="hidden" name="email" value="{{ $email}}">
-												<input type="password" class="form-control" name="password" placeholder="Password">
-											</div>
-											<div class="form-group">
-												<button type="button" class="btn btn-secondary btn-block" id="submitbutton" onclick="login();return false;"><i class="fa fa-unlock-alt"></i> Unlock</button>
-												<a href="{{ route('admin.logout') }}" class="btn btn-primary btn-block"><i class="fa fa-users"></i> Switch Account</a>
+									<form id="adminLogin">
+										<div class="card">
+											<div class="card-body">
+												<div class="text-center mb-6">
+													<img src="{{URL::asset('assets/images/brand/logo2.png')}}" class="" alt="">
+												</div>
+												<div class="text-center mb-4 ">
+													<img src="{{URL::asset('assets/images/users/avatars/avatar4.png')}}" alt="user-img" class="avatar avatar-xl brround mCS_img_loaded">
+												</div>
+												<span class="m-4 d-none d-lg-block text-center">												
+													<span class="h4"><strong>{{ $name }}</strong></span>
+													<p class="text-muted"><i class="fa fa-lock"></i> Lock Screen</p>
+												</span>
+												<div class="form-group">
+													<input type="hidden" name="email" value="{{ $email}}">
+													<input type="password" class="form-control" name="password" placeholder="Password">
+												</div>
+												<div class="form-group">
+													<button type="button" class="btn btn-secondary btn-block" id="submitbutton" onclick="login();return false;"><i class="fa fa-unlock-alt"></i> Unlock</button>
+													<a href="{{ route('admin.logout') }}" class="btn btn-primary btn-block"><i class="fa fa-users"></i> Switch Account</a>
+												</div>
 											</div>
 										</div>
-									</div>
+									</form>
 								</div>
 							</div>
 						</div>
@@ -89,11 +91,11 @@
         return false;
     }
 
-	$(document).on('keypress',function(e) {
-        if(e.which == 13) {
-            login();
-        }
-    });
+	$(document).on('keypress', '#adminLogin input', function (e) {
+    if (e.which === 13) {
+        login();
+        e.preventDefault(); 
+    }});
 
 
 </script>
