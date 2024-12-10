c539f8092480bce0ed39f70e8d0aac8b50285a71
rifqird <rdzulfikri@gmail.com>
2024-12-10 14:57:40 +0700

    daily labor multiple sheet

--- routes/web.php
+++ routes/web.php
@@ -346,6 +346,7 @@ Route::group(['middleware' => ['auth.admin', 'lock'], 'prefix' => 'hris','namesp
     Route::post('rekapperhitunganpayroll/export_excel_daily_labor',['as'=>'hris.rekapperhitunganpayroll.export_excel_daily_labor','uses'=> 'RekapPerhitunganPayrollController@export_excel_daily_labor']);
     Route::post('rekapperhitunganpayroll/proses_payroll_harian',['as'=>'hris.rekapperhitunganpayroll.proses_payroll_harian','uses'=> 'RekapPerhitunganPayrollController@proses_payroll_harian']);
     Route::post('rekapperhitunganpayroll/recap_labor_cost',['as'=>'hris.rekapperhitunganpayroll.recap_labor_cost','uses'=> 'RekapPerhitunganPayrollController@recap_labor_cost']);
+    Route::post('rekapperhitunganpayroll/recap_labor_cost_2',['as'=>'hris.rekapperhitunganpayroll.recap_labor_cost_2','uses'=> 'RekapPerhitunganPayrollController@recap_labor_cost_2']);
 
 
     Route::get('dataclosingpayroll/index',['as'=>'hris.dataclosingpayroll.index','uses'=>'DataClosingPayrollController@index']);
