82f182d6c09293860e5d62eb801edea2adb068bb
fadli rafidan <rafidfid28@gmail.com>
2025-01-02 13:34:48 +0700

    ubah logika IKS dan DT

--- app/Http/Controllers/Hris/MdAbsenHadirController.php
+++ app/Http/Controllers/Hris/MdAbsenHadirController.php
@@ -995,16 +995,21 @@ class MdAbsenHadirController extends AdminBaseController
                         if($q->status_absen=='M'){
                             $kode_ijin_payroll='M';
                         }else if($q->status_absen=='IKS'){
-                            if($q->jumlah_menit_absen_dt!=0 && $q->jumlah_menit_absen_pc==0){
-                                $kode_ijin_payroll='DT';
-                            }else if($q->jumlah_menit_absen_dt==0 && $q->jumlah_menit_absen_pc!=0){
-                                $kode_ijin_payroll='PC';
-                            }else if($q->jumlah_menit_absen_dt!=0 && $q->jumlah_menit_absen_pc!=0){
-                                $kode_ijin_payroll='DTPC';
-                            }else{
-                                $kode_ijin_payroll='OK';
-                            }
-                        }else if($q->status_absen=='LP'){
+                            if($q->absen_masuk_kerja!=null && $q->absen_pulang_kerja!=null){
+                                if($q->jumlah_menit_absen_dt!=0 && $q->jumlah_menit_absen_pc==0){
+                                    $kode_ijin_payroll='DT';
+                                    }else if($q->jumlah_menit_absen_dt==0 && $q->jumlah_menit_absen_pc!=0){
+                                        $kode_ijin_payroll='PC';
+                                    }else if($q->jumlah_menit_absen_dt!=0 && $q->jumlah_menit_absen_pc!=0){
+                                        $kode_ijin_payroll='DTPC';
+                                    }else{
+                                        $kode_ijin_payroll='OK';
+                                    }
+                                }
+                                else{
+                                    $kode_ijin_payroll='OK';
+                                }
+                         }else if($q->status_absen=='LP'){
                             $kode_ijin_payroll='LP';
                         }else if($q->status_absen=='S'){
                             $kode_ijin_payroll='S';
@@ -1494,13 +1499,18 @@ class MdAbsenHadirController extends AdminBaseController
                 if($Kehadiran->status_absen=='M'){
                     $kode_ijin_payroll='M';
                 }else if($Kehadiran->status_absen=='IKS'){
-                    if($Kehadiran->jumlah_menit_absen_dt!=0 && $Kehadiran->jumlah_menit_absen_pc==0){
-                        $kode_ijin_payroll='DT';
-                    }else if($Kehadiran->jumlah_menit_absen_dt==0 && $Kehadiran->jumlah_menit_absen_pc!=0){
-                        $kode_ijin_payroll='PC';
-                    }else if($Kehadiran->jumlah_menit_absen_dt!=0 && $Kehadiran->jumlah_menit_absen_pc!=0){
-                        $kode_ijin_payroll='DTPC';
-                    }else{
+                    if($Kehadiran->absen_masuk_kerja!=null && $Kehadiran->absen_pulang_kerja!=null){
+                        if($Kehadiran->jumlah_menit_absen_dt!=0 && $Kehadiran->jumlah_menit_absen_pc==0){
+                            $kode_ijin_payroll='DT';
+                        }else if($Kehadiran->jumlah_menit_absen_dt==0 && $Kehadiran->jumlah_menit_absen_pc!=0){
+                            $kode_ijin_payroll='PC';
+                        }else if($Kehadiran->jumlah_menit_absen_dt!=0 && $Kehadiran->jumlah_menit_absen_pc!=0){
+                            $kode_ijin_payroll='DTPC';
+                        }else{
+                            $kode_ijin_payroll='OK';
+                        }
+                        }
+                    else{
                         $kode_ijin_payroll='OK';
                     }
                 }else if($Kehadiran->status_absen=='LP'){
