02aa48df54d79c96f39f578aa3378eacf03c2d20
rifqird <rdzulfikri@gmail.com>
2025-02-18 08:36:29 +0700

    tambah route export excel transportasi

--- routes/web.php
+++ routes/web.php
@@ -199,6 +199,7 @@ Route::group(['middleware' => ['auth.admin', 'lock'], 'prefix' => 'hris','namesp
     Route::post('ga/approve_this_car_request',['as'=>'hris.ga.approve_this_car_request','uses'=>'GAController@approve_this_car_request']);
     Route::post('ga/reject_car_request',['as'=>'hris.ga.reject_car_request','uses'=>'GAController@reject_car_request']);
     Route::get('ga/print_penugasan_transportasi',['as'=>'hris.ga.print_penugasan_transportasi','uses'=>'GAController@print_penugasan_transportasi']);
+    Route::post('ga/export_excel_transportasi',['as'=>'hris.ga.export_excel_transportasi','uses'=>'GAController@export_excel_transportasi']);
     Route::get('ga/lihat_detail',['as'=>'hris.ga.lihat_detail','uses'=>'GAController@lihat_detail']);
     Route::get('ga/edit_detail',['as'=>'hris.ga.edit_detail','uses'=>'GAController@edit_detail']);
     Route::post('ga/get_route_from_user',['as'=>'hris.ga.get_route_from_user','uses'=>'GAController@get_route_from_user']);
