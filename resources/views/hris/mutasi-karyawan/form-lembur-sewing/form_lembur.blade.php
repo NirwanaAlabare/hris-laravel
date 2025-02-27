d56955dbeb0b18a13c6798d56a4e92c1f8e7fb5e
fadli rafidan <rafidfid28@gmail.com>
2025-02-27 17:05:53 +0700

    refactor: add margin to PDF export button for improved spacing in non-sewing and sewing overtime forms

--- resources/views/hris/mutasi-karyawan/form-lembur-sewing/form_lembur.blade.php
+++ resources/views/hris/mutasi-karyawan/form-lembur-sewing/form_lembur.blade.php
@@ -638,7 +638,7 @@
                                     <a class='btn btn-secondary btn-sm' onclick="export_spl(` + row.id + `,'` + row.no_form + `')">
                                                 <i class='fa fa-print'></i>
                                     </a>
-                                    <a class='btn btn-danger btn-sm' onclick="export_pdf_insentif('` + row.no_form + `')" title="reward">
+                                    <a class='btn btn-danger btn-sm ml-2' onclick="export_pdf_insentif('` + row.no_form + `')" title="reward">
                                         <i class="fa fa-file-pdf-o" aria-hidden="true" ></i>
                                     </a>
                                 </div>
