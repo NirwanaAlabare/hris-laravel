d5a7f05df28aef2d2c690c5fbdc280a34e41d0f8
fadli rafidan <rafidfid28@gmail.com>
2025-02-27 16:15:34 +0700

    refactor: adjust minimum month calculation to include the 27th day for payroll processing

--- app/Http/Controllers/Hris/RekapPerhitunganPayrollController.php
+++ app/Http/Controllers/Hris/RekapPerhitunganPayrollController.php
@@ -682,7 +682,7 @@ class RekapPerhitunganPayrollController extends AdminBaseController
         $today = date('d');
 
         $minMonth = $this->month;
-        if ($today >= 28 && $today <= 31) {
+        if ($today >= 27 && $today <= 31) {
             $minMonth = date('Y-m', strtotime(date('Y-m-01') . ' +1 month'));
         }
 
