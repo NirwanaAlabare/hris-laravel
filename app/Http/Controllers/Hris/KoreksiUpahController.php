e168fd2fe881200c39606cb9c89e1fae73bb3834
fadli rafidan <rafidfid28@gmail.com>
2025-04-28 16:38:27 +0700

    fix: refine update query in KoreksiUpahController to include jenis_koreksi condition

--- app/Http/Controllers/Hris/KoreksiUpahController.php
+++ app/Http/Controllers/Hris/KoreksiUpahController.php
@@ -546,7 +546,7 @@ class KoreksiUpahController extends AdminBaseController
                             'jenis_koreksi' => $value['jenis_koreksi'],
 
                         ];
-                        DataKoreksiUpah::where('enroll_id',$value['enroll_id'])->where('periode_tanggal_koreksi',$value['periode_tanggal_koreksi'])->update($data_update);
+                        DataKoreksiUpah::where('enroll_id',$value['enroll_id'])->where('periode_tanggal_koreksi',$value['periode_tanggal_koreksi'])->where('jenis_koreksi',$value['jenis_koreksi'])->update($data_update);
                     }
                     else{
                         $data_insert=[
