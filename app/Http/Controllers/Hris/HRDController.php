62f69e09a2cfa8c7966d7fa509223f1693806f03
fadli rafidan <rafidfid28@gmail.com>
2025-05-13 16:34:42 +0700

    fix: add department data to kontrak kerja view for improved employee contract management

--- app/Http/Controllers/Hris/HRDController.php
+++ app/Http/Controllers/Hris/HRDController.php
@@ -16,6 +16,7 @@ use App\Imports\KontrakKerjaImportToDatabase;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 use App\Models\MasterDataAbsenKehadiran;
+use App\Models\DepartmentAll;
 use App\Exports\exportExcelKontrak;
 use App\Models\DasarPotBPJS;
 use DateTime;
@@ -378,7 +379,8 @@ class HRDController extends AdminBaseController
     public function kontrak_kerja(){
         $selectEmployee =  EmployeeAtribut::selectRaw('enroll_id, nik, employee_name, concat(enroll_id, " - ", nik, " - ", employee_name) select_employee')->groupby('enroll_id')->orderby('employee_name', 'asc')->get();
         $selectNoKTP = EmployeeAtribut::selectRaw('nomor_ktp')->groupby('nomor_ktp')->orderby('nomor_ktp', 'asc')->get();
-        return View::make('hris/hrd/kontrak_kerja',compact('selectEmployee','selectNoKTP'), $this->data);
+        $department =DepartmentAll::select('department_name')->distinct()->orderBy('department_id')->groupBy('department_id')->get();
+        return View::make('hris/hrd/kontrak_kerja',compact('selectEmployee','selectNoKTP','department'), $this->data);
     }
     public function get_employee_contract(){
         $inSearchVariable='';
