65f05dc218811700a1ec8e7b805c3d6d72876f2a
fadli rafidan <rafidfid28@gmail.com>
2025-05-14 16:21:39 +0700

    fix: adjust table cell width for barcode display in kontrak kerja views

--- resources/views/hris/Laporan/kontrak_kerja_karyawan_2.blade.php
+++ resources/views/hris/Laporan/kontrak_kerja_karyawan_2.blade.php
@@ -147,7 +147,7 @@
     <table width="562" style="padding-top: 100px">
         <thead>
             <tr>
-                <td style="border-bottom:1px solid black;height:24px" width="93%"></td>
+                <td style="border-bottom:1px solid black;height:24px" width="88%"></td>
                 <td rowspan="2">
                     <img src="data:image/png;base64,{{ \DNS2D::getBarcodePNG(url('hris/identity/card_employee_form_identity') . '?enroll_id=' . $value->enroll_id . '&no_form=' . urlencode($no_form) . '&type=SK_KERJA', 'QRCODE') }}" alt="barcode" style="width: 60px; height: 60px;background-color:white" />
                 </td>
