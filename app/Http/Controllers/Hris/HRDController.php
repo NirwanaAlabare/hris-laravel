f0d89e2b733e39004e379e0573351520a50bb491
fadli rafidan <rafidfid28@gmail.com>
2025-05-27 10:56:31 +0700

    fix: update contract PDF views to dynamically display employee work hours

--- app/Http/Controllers/Hris/HRDController.php
+++ app/Http/Controllers/Hris/HRDController.php
@@ -738,7 +738,50 @@ class HRDController extends AdminBaseController
     public function print_pdf_kontrak(){
         $enroll_id=request()->enroll_id;
         $no_form=request()->no_form;
-        $data=DB::select("select a.status_staff,a.enroll_id,a.nik,a.employee_name,a.status_jabatan,a.sub_dept_name,a.department_name,a.status_kontrak_tetap,a.status_aktif,a.join_date,a.tanggal_resign,a.nomor_ktp,a.tempat_lahir,a.alamat_rumah,a.tanggal_lahir,a.no_surat,b.contract,b.contract_end,c.max_contract,c.max_contract_end from employee_atribut a left join employee_contract b on a.enroll_id=b.enroll_id left join (select enroll_id,max(contract) max_contract,max(contract_end) max_contract_end from employee_contract)c on a.enroll_id=c.enroll_id where a.enroll_id=".$enroll_id."");
+        // $data=DB::select("select a.status_staff,a.enroll_id,a.nik,a.employee_name,a.status_jabatan,a.sub_dept_name,a.department_name,a.status_kontrak_tetap,a.status_aktif,a.join_date,a.tanggal_resign,a.nomor_ktp,a.tempat_lahir,a.alamat_rumah,a.tanggal_lahir,a.no_surat,b.contract,b.contract_end,c.max_contract,c.max_contract_end from employee_atribut a left join employee_contract b on a.enroll_id=b.enroll_id left join (select enroll_id,max(contract) max_contract,max(contract_end) max_contract_end from employee_contract)c on a.enroll_id=c.enroll_id where a.enroll_id=".$enroll_id."");
+        $data=DB::select("
+                    SELECT
+                    a.status_staff,
+                    a.enroll_id,
+                    a.nik,
+                    a.employee_name,
+                    a.status_jabatan,
+                    a.sub_dept_name,
+                    a.department_name,
+                    a.status_kontrak_tetap,
+                    a.status_aktif,
+                    a.join_date,
+                    a.tanggal_resign,
+                    a.nomor_ktp,
+                    a.tempat_lahir,
+                    a.alamat_rumah,
+                    a.tanggal_lahir,
+                    a.no_surat,
+                    b.contract,
+                    b.contract_end,
+                    c.max_contract,
+                    c.max_contract_end,
+                    mda.*
+                    FROM employee_atribut a
+                    LEFT JOIN employee_contract b ON a.enroll_id = b.enroll_id
+                    LEFT JOIN (
+                    SELECT
+                        enroll_id,
+                        MAX(contract) AS max_contract,
+                        MAX(contract_end) AS max_contract_end
+                    FROM employee_contract
+                    GROUP BY enroll_id
+                    ) c ON a.enroll_id = c.enroll_id
+                    LEFT JOIN (
+                    SELECT mulai_jam_kerja, akhir_jam_kerja, tanggal_berjalan, enroll_id
+                    FROM master_data_absen_kehadiran
+                    WHERE (enroll_id, tanggal_berjalan) IN (
+                        SELECT enroll_id, MAX(tanggal_berjalan)
+                        FROM master_data_absen_kehadiran
+                        GROUP BY enroll_id
+                    )
+                    ) mda ON a.enroll_id = mda.enroll_id
+                    WHERE  a.enroll_id=".$enroll_id."");
         $umk=DasarPotBPJS::orderBy('created_at','desc')->limit(1)->first()->dasar_pot_bpjs_rupiah;
         $fileName='Kontrak Kerja '.$data[0]->employee_name.'('.request()->enroll_id.') '.$data[0]->max_contract_end.' '.date('His');
         $pdf = PDF::loadView('hris.laporan.kontrak_kerja_karyawan',["no_form"=>$no_form,"data" => $data,"umk"=>$umk])->setPaper('A4', 'fotrait')->stream($fileName.'.pdf');
@@ -749,7 +792,16 @@ class HRDController extends AdminBaseController
         $no_form=request()->no_form;
         $contract=request()->contract;
         $contract_end=request()->contract_end;
-        $data=DB::select("select a.status_staff,a.enroll_id,a.nik,a.employee_name,a.status_jabatan,a.sub_dept_name,a.department_name,a.status_kontrak_tetap,a.status_aktif,a.join_date,a.tanggal_resign,a.nomor_ktp,a.tempat_lahir,a.alamat_rumah,a.tanggal_lahir,a.no_surat,b.contract,b.contract_end,c.max_contract,c.max_contract_end from employee_atribut a left join employee_contract b on a.enroll_id=b.enroll_id left join (select enroll_id,max(contract) max_contract,max(contract_end) max_contract_end from employee_contract group by enroll_id)c on a.enroll_id=c.enroll_id where a.enroll_id=".$enroll_id." group by a.enroll_id");
+        $data=DB::select("select a.status_staff,c.max_contract,mda.*,
+                    c.max_contract_end,a.enroll_id,a.nik,a.employee_name,a.status_jabatan,a.sub_dept_name,a.department_name,a.status_kontrak_tetap,a.status_aktif,a.join_date,a.tanggal_resign,a.nomor_ktp,a.tempat_lahir,a.alamat_rumah,a.tanggal_lahir,a.no_surat,b.contract,b.contract_end,c.max_contract,c.max_contract_end from employee_atribut a left join employee_contract b on a.enroll_id=b.enroll_id left join (select enroll_id,max(contract) max_contract,max(contract_end) max_contract_end from employee_contract group by enroll_id)c on a.enroll_id=c.enroll_id LEFT JOIN (
+                    SELECT mulai_jam_kerja, akhir_jam_kerja, tanggal_berjalan, enroll_id
+                    FROM master_data_absen_kehadiran
+                    WHERE (enroll_id, tanggal_berjalan) IN (
+                        SELECT enroll_id, MAX(tanggal_berjalan)
+                        FROM master_data_absen_kehadiran
+                        GROUP BY enroll_id
+                    )
+                    ) mda ON a.enroll_id = mda.enroll_id where a.enroll_id=".$enroll_id." group by a.enroll_id");
         $umk=DasarPotBPJS::orderBy('created_at','desc')->limit(1)->first()->dasar_pot_bpjs_rupiah;
         $fileName='Kontrak Kerja '.$data[0]->employee_name.'('.request()->enroll_id.') '.$contract_end.' '.date('His');
         $pdf = PDF::loadView('hris.laporan.kontrak_kerja_karyawan_2',["no_form"=>$no_form,"contract2"=>$contract,"contract_end2"=>$contract_end,"data" => $data,"umk"=>$umk])->setPaper('A4', 'fotrait')->stream($fileName.'.pdf');
@@ -866,19 +918,96 @@ class HRDController extends AdminBaseController
         $no_form = request()->no_form;
 
         // Ambil semua kontrak untuk semua enroll_id
-        $data = DB::table('employee_atribut as a')
-            ->leftJoin('employee_contract as b', 'a.enroll_id', '=', 'b.enroll_id')
-            ->select(
-                'a.status_staff','a.enroll_id','a.nik','a.employee_name','a.status_jabatan',
-                'a.sub_dept_name','a.department_name','a.status_kontrak_tetap','a.status_aktif',
-                'a.join_date','a.tanggal_resign','a.nomor_ktp','a.tempat_lahir','a.alamat_rumah',
-                'a.tanggal_lahir','a.no_surat','b.contract','b.contract_end'
-            )
-            ->whereIn('a.enroll_id', $enroll_id)
-            ->get();
+        // $data = DB::table('employee_atribut as a')
+        //     ->leftJoin('employee_contract as b', 'a.enroll_id', '=', 'b.enroll_id')
+        //     ->select(
+        //         'a.status_staff','a.enroll_id','a.nik','a.employee_name','a.status_jabatan',
+        //         'a.sub_dept_name','a.department_name','a.status_kontrak_tetap','a.status_aktif',
+        //         'a.join_date','a.tanggal_resign','a.nomor_ktp','a.tempat_lahir','a.alamat_rumah',
+        //         'a.tanggal_lahir','a.no_surat','b.contract','b.contract_end'
+        //     )
+        //     ->whereIn('a.enroll_id', $enroll_id)
+        //     ->get();
+
+        // $data = DB::table('employee_atribut as a')
+        //         ->leftJoin('employee_contract as b', 'a.enroll_id', '=', 'b.enroll_id')
+        //         ->leftJoinSub(
+        //             DB::table('master_data_absen_kehadiran')
+        //                 ->select('mulai_jam_kerja', 'akhir_jam_kerja', 'tanggal_berjalan', 'enroll_id')
+        //                 ->whereIn(DB::raw('(enroll_id, tanggal_berjalan)'), function($query) {
+        //                     $query->select(DB::raw('enroll_id, MAX(tanggal_berjalan)'))
+        //                         ->from('master_data_absen_kehadiran')
+        //                         ->groupBy('enroll_id');
+        //                 }),
+        //             'mda',
+        //             function ($join) {
+        //                 $join->on('a.enroll_id', '=', 'mda.enroll_id');
+        //             }
+        //         )
+        //         ->select(
+        //             'a.status_staff','a.enroll_id','a.nik','a.employee_name','a.status_jabatan',
+        //             'a.sub_dept_name','a.department_name','a.status_kontrak_tetap','a.status_aktif',
+        //             'a.join_date','a.tanggal_resign','a.nomor_ktp','a.tempat_lahir','a.alamat_rumah',
+        //             'a.tanggal_lahir','a.no_surat','b.contract','b.contract_end',
+        //             'mda.mulai_jam_kerja','mda.akhir_jam_kerja','mda.tanggal_berjalan'
+        //         )
+        //         ->whereIn('a.enroll_id', $enroll_id)
+        //         ->get();
+
+     // 1. Ambil data karyawan dan kontrak
+       $employee = DB::table('employee_atribut as a')
+    ->select(
+        'a.status_staff','a.enroll_id','a.nik','a.employee_name','a.status_jabatan',
+        'a.sub_dept_name','a.department_name','a.status_kontrak_tetap','a.status_aktif',
+        'a.join_date','a.tanggal_resign','a.nomor_ktp','a.tempat_lahir','a.alamat_rumah',
+        'a.tanggal_lahir','a.no_surat'
+    )
+    ->whereIn('a.enroll_id', $enroll_id)
+    ->get()
+    ->keyBy('enroll_id');
+
+
+        // 2. Ambil absen terakhir per enroll_id
+      $contract = DB::table('employee_contract as ec1')
+    ->select('ec1.enroll_id', 'ec1.contract', 'ec1.contract_end')
+    ->join(DB::raw('(
+        SELECT enroll_id, MAX(contract_end) as latest_contract_end
+        FROM employee_contract
+        GROUP BY enroll_id
+    ) as latest'), function($join) {
+        $join->on('ec1.enroll_id', '=', 'latest.enroll_id');
+        $join->on('ec1.contract_end', '=', 'latest.latest_contract_end');
+    })
+    ->whereIn('ec1.enroll_id', $enroll_id)
+    ->get()
+    ->keyBy('enroll_id');
+$absen = DB::table('master_data_absen_kehadiran as m1')
+    ->select('m1.enroll_id', 'm1.mulai_jam_kerja', 'm1.akhir_jam_kerja', 'm1.tanggal_berjalan')
+    ->join(DB::raw('(SELECT enroll_id, MAX(tanggal_berjalan) as latest_date
+                    FROM master_data_absen_kehadiran
+                    GROUP BY enroll_id) as m2'), function($join) {
+        $join->on('m1.enroll_id', '=', 'm2.enroll_id');
+        $join->on('m1.tanggal_berjalan', '=', 'm2.latest_date');
+    })
+    ->whereIn('m1.enroll_id', $enroll_id)
+    ->get()
+    ->keyBy('enroll_id');
+$final = $employee->map(function ($item, $key) use ($contract, $absen) {
+    $item->contract = $contract[$key]->contract ?? null;
+    $item->contract_end = $contract[$key]->contract_end ?? null;
+
+    $item->mulai_jam_kerja = $absen[$key]->mulai_jam_kerja ?? null;
+    $item->akhir_jam_kerja = $absen[$key]->akhir_jam_kerja ?? null;
+    $item->tanggal_berjalan = $absen[$key]->tanggal_berjalan ?? null;
+
+    return $item;
+})->values();
+
+
+
 
         // Ambil kontrak terakhir per enroll_id
-        $latest_per_employee = $data->groupBy('enroll_id')->map(function ($contracts) {
+        $latest_per_employee = $final->groupBy('enroll_id')->map(function ($contracts) {
             return $contracts->sortByDesc('contract_end')->first();
         })->values();
 
