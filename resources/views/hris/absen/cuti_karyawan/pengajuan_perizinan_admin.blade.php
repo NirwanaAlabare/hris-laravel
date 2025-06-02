ccd38c3740efabc032ddee6de96aee4b71bd3c94
fadli rafidan <rafidfid28@gmail.com>
2025-06-02 10:21:31 +0700

    fix: comment out redundant notification logic in perizinan update process

--- resources/views/hris/absen/cuti_karyawan/pengajuan_perizinan_admin.blade.php
+++ resources/views/hris/absen/cuti_karyawan/pengajuan_perizinan_admin.blade.php
@@ -1939,42 +1939,44 @@ h1 {
                                             swal("", "update perizinan gagal", "error");
                                         }
                                     });
-                                } else if(res.length > 0 && res[0].nomor_form_perizinan && res[0].is_verifikasi_pengajuan_admin == 1){
-                                        notif({
-                                            msg: `<b>Info:</b> Perijinan sudah dibuat oleh ${res[0].operator}.`,
-                                            type: "info",
-                                            position: "center",
-                                            width: 800,
-                                        });
-                                        $('#progress-show-1').hide();
-                                        $('#progress-hide-1').show();
-                                        $('#btn-save-izin').removeClass("btn-loading");
-                                        $("#btn-save-izin").html('<span><i class="fa fa-save"></i></span> Save');
-                                        $("#form1 :input").prop("disabled", false);
-                                        $("#btn-save-izin").prop("disabled", false);
-                                        $("#btn-save-iks").prop("disabled", false);
-                                        $("#btn-cancel-izin").prop("disabled", false);
-                                        $("#btn-cancel-iks").prop("disabled", false);
-                                        return false;
-                                }else if(res.length > 0 && !res[0].nomor_form_perizinan){
-                                        notif({
-                                            msg: `<b>Info:</b> Perijinan pada tanggal tersebut sudah dibuat oleh ${res[0].operator}, menunggu Approval.`,
-                                            type: "info",
-                                            position: "center",
-                                            width: 800,
-                                        });
-                                        $('#progress-show-1').hide();
-                                        $('#progress-hide-1').show();
-                                        $('#btn-save-izin').removeClass("btn-loading");
-                                        $("#btn-save-izin").html('<span><i class="fa fa-save"></i></span> Save');
-                                        $("#form1 :input").prop("disabled", false);
-                                        $("#btn-save-izin").prop("disabled", false);
-                                        $("#btn-save-iks").prop("disabled", false);
-                                        $("#btn-cancel-izin").prop("disabled", false);
-                                        $("#btn-cancel-iks").prop("disabled", false);
-                                        return false;
-                                } else if(res.length  == 0 ){
-                                $.ajax({
+                                }
+                                // else if(res.length > 0 && res[0].nomor_form_perizinan && res[0].is_verifikasi_pengajuan_admin == 1){
+                                //         notif({
+                                //             msg: `<b>Info:</b> Perijinan sudah dibuat oleh ${res[0].operator}.`,
+                                //             type: "info",
+                                //             position: "center",
+                                //             width: 800,
+                                //         });
+                                //         $('#progress-show-1').hide();
+                                //         $('#progress-hide-1').show();
+                                //         $('#btn-save-izin').removeClass("btn-loading");
+                                //         $("#btn-save-izin").html('<span><i class="fa fa-save"></i></span> Save');
+                                //         $("#form1 :input").prop("disabled", false);
+                                //         $("#btn-save-izin").prop("disabled", false);
+                                //         $("#btn-save-iks").prop("disabled", false);
+                                //         $("#btn-cancel-izin").prop("disabled", false);
+                                //         $("#btn-cancel-iks").prop("disabled", false);
+                                //         return false;
+                                // }else if(res.length > 0 && !res[0].nomor_form_perizinan){
+                                //         notif({
+                                //             msg: `<b>Info:</b> Perijinan pada tanggal tersebut sudah dibuat oleh ${res[0].operator}, menunggu Approval.`,
+                                //             type: "info",
+                                //             position: "center",
+                                //             width: 800,
+                                //         });
+                                //         $('#progress-show-1').hide();
+                                //         $('#progress-hide-1').show();
+                                //         $('#btn-save-izin').removeClass("btn-loading");
+                                //         $("#btn-save-izin").html('<span><i class="fa fa-save"></i></span> Save');
+                                //         $("#form1 :input").prop("disabled", false);
+                                //         $("#btn-save-izin").prop("disabled", false);
+                                //         $("#btn-save-iks").prop("disabled", false);
+                                //         $("#btn-cancel-izin").prop("disabled", false);
+                                //         $("#btn-cancel-iks").prop("disabled", false);
+                                //         return false;
+                                // }
+                                else {
+                                    $.ajax({
                                         type:"POST",
                                         url: "{{route('cuti_karyawan.create_perizinan_menu_admin')}}",
                                         dataType: 'json',
