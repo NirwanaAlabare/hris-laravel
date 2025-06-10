00be3339f222f078ef7d9723182feb274d6195ee
fadli rafidan <rafidfid28@gmail.com>
2025-06-10 11:25:16 +0700

    refactor: Improve attendance report layout and optimize data handling in rincian_kehadiran_karyawan view

--- resources/views/hris/Laporan/rincian_kehadiran_karyawan.blade.php
+++ resources/views/hris/Laporan/rincian_kehadiran_karyawan.blade.php
@@ -1,138 +1,126 @@
+<?php $no = 0; ?>
+@foreach ($employee as $key => $value)
+    <?php $no++; ?>
+    @if($no != count($employee))
+        <div class="wrapper-page">
+    @else
+        <div>
+    @endif
 
-<?php $no=0; ?>
-@foreach ($employee as $key=> $value)
-<?php $no++; ?>
-@if($no!=count($employee))
-<div class="wrapper-page">
-@else
-<div>
-@endif
-	<table style="font-family: Arial, Helvetica, sans-serif;letter-spacing: 1px; width: 920px; margin: 0 auto;">
-		<tr>
-			<td style="font-weight: bold">PT. NIRWANA ALABARE GARMENT</td>
-		</tr>
-		<tr>
-			<td style="font-size: 10pt;font-family: Arial, Helvetica, sans-serif; letter-spacing: 1px;">Laporan Rinci Kehadiran Karyawan</td>
-		</tr>
-		<tr>
-			<td style="height: 10px"></td>
-		</tr>
-		<tr>
-			<td width="520" style="border-bottom: 1px solid black;font-size: 10pt">{{$tanggal_awal_absen}} - {{$tanggal_akhir_absen}}</td>
-		</tr>
-	</table>
-	<table class="textkecil" style="font-size: 10pt;font-family: Arial, Helvetica, sans-serif; letter-spacing: 1px; width: 920px; margin: 0 auto;">
-		<tr>
-			<td style="height: 10px"></td>
-		</tr>
-		<tr>
-			<td width="90">NIP</td>
-			<td width="90">: {{$value->nik}}</td>
-			<td width="90">JABATAN</td>
-			<td>: {{$value->status_jabatan}}</td>
-		</tr>
-		<tr>
-			<td>NAMA KARYAWAN</td>
-			<td>: {{$value->employee_name}}</td>
-			<td>BAGIAN</td>
-			<td>: {{$value->sub_dept_name}}</td>
-		</tr>
-		<tr>
-			<td style="height: 10px"></td>
-		</tr>
-	</table>
-	<table border="1" class="textkecilbanget" style="font-size: 10pt;font-family: Arial, Helvetica, sans-serif; letter-spacing: 1px; width: 100%; margin: 0 auto;">
-		<tr>
-			<td height="30" width="120">&nbsp;Tanggal</td>
-			<td width="90">&nbsp;Hari</td>
-			<td >&nbsp;Keterangan</td>
-			<td >&nbsp;Jam masuk</td>
-			<td >&nbsp;Jam keluar</td>
-			<td >&nbsp;DT</td>
-			<td >&nbsp;PC</td>
-			<td >&nbsp;Lembur 1</td>
-			<td >&nbsp;Lembur 2</td>
-			<td >&nbsp;Lembur 3</td>
-			<td >&nbsp;Lembur 4</td>
-			<td >&nbsp;Total Lembur</td>
-		</tr>
-		@foreach ($value->absensi as $val)
-		<?php
-		$tanggal_berjalan=Carbon\Carbon::parse($val->tanggal_berjalan)->translatedFormat('d F Y');
-		$absen_masuk=substr($val->absen_masuk_kerja,0,5);
-		$absen_pulang=substr($val->absen_pulang_kerja,0,5);
-		$jumlah_menit_absen_dt='';
-		if($val->jumlah_menit_absen_dt!=0){
-			$jumlah_menit_absen_dt=$val->jumlah_menit_absen_dt;
-		}
-		$jumlah_menit_absen_pc='';
-		if($val->jumlah_menit_absen_pc!=0){
-			$jumlah_menit_absen_pc=$val->jumlah_menit_absen_pc;
-		}
-		$lembur1='';
-		if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_1')[0]) && $val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_1')[0]!=0){
-			$lembur1=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_1')[0];
-		}
-		$lembur2='';
-		if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_2')[0]) && $val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_2')[0]!=0){
-			$lembur2=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_2')[0];
-		}
-		$lembur3='';
-		if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_3')[0]) && $val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_3')[0]!=0){
-			$lembur3=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_3')[0];
-		}
-		$lembur4='';
-		if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_4')[0]) && $val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_4')[0]!=0){
-			$lembur4=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('lembur_4')[0];
-		}
-		$total_lembur_1234='';
-		if(isset($val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('total_lembur_1234')[0])){
-			$total_lembur_1234=$val->rekap_lembur->where('tanggal_berjalan',$val['tanggal_berjalan'])->pluck('total_lembur_1234')[0];
-		}
-		?>
-		<tr>
-			<td height="30">&nbsp;{{$tanggal_berjalan}}</td>
-			<td>&nbsp;{{$val->nama_hari}}</td>
-			<td>&nbsp;{{$val->status_absen}}</td>
-			<td align="center">{{$absen_masuk}}</td>
-			<td align="center">{{$absen_pulang}}</td>
-			<td align="center">{{$jumlah_menit_absen_dt}}</td>
-			<td align="center">{{$jumlah_menit_absen_pc}}</td>
-			<td align="center">{{$lembur1}}</td>
-			<td align="center">{{$lembur2}}</td>
-			<td align="center">{{$lembur3}}</td>
-			<td align="center">{{$lembur4}}</td>
-			<td align="center">{{$total_lembur_1234}}</td>
-		</tr>
-		@endforeach
-		<tr>
-			<td colspan="5" height="30" style="background-color:#fffc04;font-weight:bold">GRAND TOTAL</td>
-			<td style="background-color:#fffc04;font-weight:bold;text-align:center">@if($jumlah_menit[$key]['total_menit_absen_dt']==0) - @else {{$jumlah_menit[$key]['total_menit_absen_dt']}} @endif</td>
-			<td style="background-color:#fffc04;font-weight:bold;text-align:center">@if($jumlah_menit[$key]['total_menit_absen_pc']==0) - @else {{$jumlah_menit[$key]['total_menit_absen_pc']}} @endif</td>
-			<td style="background-color:#fffc04;font-weight:bold;text-align:center">@if($jumlah_menit[$key]['total_menit_lembur_1']==0) - @else {{$jumlah_menit[$key]['total_menit_lembur_1']}} @endif</td>
-			<td style="background-color:#fffc04;font-weight:bold;text-align:center">@if($jumlah_menit[$key]['total_menit_lembur_2']==0) - @else {{$jumlah_menit[$key]['total_menit_lembur_2']}} @endif</td>
-			<td style="background-color:#fffc04;font-weight:bold;text-align:center">@if($jumlah_menit[$key]['total_menit_lembur_3']==0) - @else {{$jumlah_menit[$key]['total_menit_lembur_3']}} @endif</td>
-			<td style="background-color:#fffc04;font-weight:bold;text-align:center">@if($jumlah_menit[$key]['total_menit_lembur_4']==0) - @else {{$jumlah_menit[$key]['total_menit_lembur_4']}} @endif</td>
-			<td style="background-color:#fffc04;font-weight:bold;text-align:center">@if($jumlah_menit[$key]['total_menit_lembur_1234']==0) - @else {{$jumlah_menit[$key]['total_menit_lembur_1234']}} @endif</td>
-		</tr>
-	</table>
-	<table>
-		<tr>
-			<td style="height: 10px"></td>
-		</tr>
-	</table>
-	<table class="textkecil" style="font-size: 10pt;font-family: Arial, Helvetica, sans-serif; letter-spacing: 1px;">
-		<tr>
-			<td width="120">Hari kerja</td>
-			<td>{{$jumlah_absen[$key]['hari_kerja']}}</td>
-		</tr>
-		<tr>
-			<td>Absen</td>
-			<td>{{$jumlah_absen[$key]['hari_absen']}}</td>
-		</tr>
-	</table>
+    @php
+        $absensi = $value->absensi;
+        $chunkSize = 34;
+        $chunks = $absensi->chunk($chunkSize);
+    @endphp
+
+    @foreach ($chunks as $chunkIndex => $chunk)
+        @if ($chunkIndex > 0)
+            <div style="page-break-after: always;"></div>
+        @endif
+
+        {{-- HEADER: Judul + Info Karyawan --}}
+        <table style="font-family: Arial, Helvetica, sans-serif; letter-spacing: 1px; width: 920px; margin: 0 auto;">
+            <tr><td style="font-weight: bold">PT. NIRWANA ALABARE GARMENT</td></tr>
+            <tr><td style="font-size: 10pt;">Laporan Rinci Kehadiran Karyawan</td></tr>
+            <tr><td style="height: 10px"></td></tr>
+            <tr><td width="520" style="border-bottom: 1px solid black; font-size: 10pt;">{{$tanggal_awal_absen}} - {{$tanggal_akhir_absen}}</td></tr>
+        </table>
+
+        <table class="textkecil" style="font-size: 10pt; font-family: Arial, Helvetica, sans-serif; letter-spacing: 1px; width: 920px; margin: 0 auto;">
+            <tr><td style="height: 10px"></td></tr>
+            <tr>
+                <td width="90">NIP</td><td width="90">: {{$value->nik}}</td>
+                <td width="90">JABATAN</td><td>: {{$value->status_jabatan}}</td>
+            </tr>
+            <tr>
+                <td>NAMA KARYAWAN</td><td>: {{$value->employee_name}}</td>
+                <td>BAGIAN</td><td>: {{$value->sub_dept_name}}</td>
+            </tr>
+            <tr><td style="height: 10px"></td></tr>
+        </table>
+
+        {{-- HEADER KOLOM --}}
+        <table border="1" class="textkecilbanget" style="font-size: 10pt; font-family: Arial, Helvetica, sans-serif; letter-spacing: 1px; width: 100%; margin: 0 auto;">
+            <tr>
+                <td height="30" width="120">&nbsp;Tanggal</td>
+                <td width="80">&nbsp;Hari</td>
+                <td>&nbsp;Keterangan</td>
+                <td>&nbsp;Jam masuk</td>
+                <td>&nbsp;Jam keluar</td>
+                <td>&nbsp;DT</td>
+                <td>&nbsp;PC</td>
+                <td>&nbsp;Lembur 1</td>
+                <td>&nbsp;Lembur 2</td>
+                <td>&nbsp;Lembur 3</td>
+                <td>&nbsp;Lembur 4</td>
+                <td>&nbsp;Total Lembur</td>
+            </tr>
+
+            @foreach ($chunk as $val)
+                @php
+                    $tanggal_berjalan = \Carbon\Carbon::parse($val->tanggal_berjalan)->translatedFormat('d F Y');
+                    $absen_masuk = substr($val->absen_masuk_kerja, 0, 5);
+                    $absen_pulang = substr($val->absen_pulang_kerja, 0, 5);
+                    $jumlah_menit_absen_dt = $val->jumlah_menit_absen_dt ?: '';
+                    $jumlah_menit_absen_pc = $val->jumlah_menit_absen_pc ?: '';
+
+                    $rl = $val->rekap_lembur->where('tanggal_berjalan', $val->tanggal_berjalan);
+                    $lembur1 = $rl->pluck('lembur_1')->first() ?: '';
+                    $lembur2 = $rl->pluck('lembur_2')->first() ?: '';
+                    $lembur3 = $rl->pluck('lembur_3')->first() ?: '';
+                    $lembur4 = $rl->pluck('lembur_4')->first() ?: '';
+                    $total_lembur_1234 = $rl->pluck('total_lembur_1234')->first() ?: '';
+                @endphp
+
+                <tr>
+                    <td height="30">&nbsp;{{$tanggal_berjalan}}</td>
+                    <td>&nbsp;{{$val->nama_hari}}</td>
+                    <td>&nbsp;{{$val->status_absen}}</td>
+                    <td align="center">{{$absen_masuk}}</td>
+                    <td align="center">{{$absen_pulang}}</td>
+                    <td align="center">{{$jumlah_menit_absen_dt}}</td>
+                    <td align="center">{{$jumlah_menit_absen_pc}}</td>
+                    <td align="center">{{$lembur1}}</td>
+                    <td align="center">{{$lembur2}}</td>
+                    <td align="center">{{$lembur3}}</td>
+                    <td align="center">{{$lembur4}}</td>
+                    <td align="center">{{$total_lembur_1234}}</td>
+                </tr>
+            @endforeach
+
+            {{-- Jika ini adalah halaman terakhir untuk karyawan ini, tampilkan grand total --}}
+            @if ($loop->last)
+                <tr>
+                    <td colspan="5" height="30" style="background-color:#fffc04;font-weight:bold">GRAND TOTAL</td>
+                    <td style="background-color:#fffc04;font-weight:bold;text-align:center">{{ $jumlah_menit[$key]['total_menit_absen_dt'] ?: '-' }}</td>
+                    <td style="background-color:#fffc04;font-weight:bold;text-align:center">{{ $jumlah_menit[$key]['total_menit_absen_pc'] ?: '-' }}</td>
+                    <td style="background-color:#fffc04;font-weight:bold;text-align:center">{{ $jumlah_menit[$key]['total_menit_lembur_1'] ?: '-' }}</td>
+                    <td style="background-color:#fffc04;font-weight:bold;text-align:center">{{ $jumlah_menit[$key]['total_menit_lembur_2'] ?: '-' }}</td>
+                    <td style="background-color:#fffc04;font-weight:bold;text-align:center">{{ $jumlah_menit[$key]['total_menit_lembur_3'] ?: '-' }}</td>
+                    <td style="background-color:#fffc04;font-weight:bold;text-align:center">{{ $jumlah_menit[$key]['total_menit_lembur_4'] ?: '-' }}</td>
+                    <td style="background-color:#fffc04;font-weight:bold;text-align:center">{{ $jumlah_menit[$key]['total_menit_lembur_1234'] ?: '-' }}</td>
+                </tr>
+            @endif
+        </table>
+    @endforeach
+
+    {{-- Rangkuman Kehadiran --}}
+    <table>
+        <tr><td style="height: 10px"></td></tr>
+    </table>
+    <table class="textkecil" style="font-size: 10pt; font-family: Arial, Helvetica, sans-serif; letter-spacing: 1px;">
+        <tr>
+            <td width="120">Hari kerja</td>
+            <td>{{ $jumlah_absen[$key]['hari_kerja'] }}</td>
+        </tr>
+        <tr>
+            <td>Absen</td>
+            <td>{{ $jumlah_absen[$key]['hari_absen'] }}</td>
+        </tr>
+    </table>
 </div>
 @endforeach
+
 <style>
 
 	.textkecil{
