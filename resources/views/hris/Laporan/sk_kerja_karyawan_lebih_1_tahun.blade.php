6ca058be7925abafd4cf3a651a152eeef70c7717
fadli rafidan <rafidfid28@gmail.com>
2025-06-10 13:25:02 +0700

    fix: Update employment certificate display to conditionally show notes

--- resources/views/hris/Laporan/sk_kerja_karyawan_lebih_1_tahun.blade.php
+++ resources/views/hris/Laporan/sk_kerja_karyawan_lebih_1_tahun.blade.php
@@ -67,7 +67,7 @@
                 <td colspan="5" style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;vertical-align:top;font-size:14pt" align="center"><b><u>CERTIFICATE OF EMPLOYMENT</u></b></td>
             </tr>
             <tr>
-                <td colspan="5" style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;vertical-align:top;font-size:14pt" align="center"><b>{{$no_form}}</b></td>
+                <td colspan="5" style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;vertical-align:top;font-size:14pt" align="center"><b>{{$no_form}}{{ $value->catatan ? ('/' . Str::substr($value->catatan, 0, 3)) : '' }}</b></td>
             </tr>
             <tr>
                 <td colspan="5" style="height: 0"></td>
@@ -235,7 +235,7 @@
                     </tr>
 
                     <tr>
-                          <td style="height:24px">{{ $value->catatan ? Str::substr($value->catatan, 0, 3) : '' }}</td>
+                          <td style="height:24px"></td>
                     </tr>
                 </thead>
             </table>
