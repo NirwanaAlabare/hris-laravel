c0819a99bd13da697a95db7af5be809f75cc2d03
it-nag <ramon@ptnag.com>
2025-06-16 09:43:42 +0700

    uabh style

--- resources/views/hris/Laporan/id_card_department.blade.php
+++ resources/views/hris/Laporan/id_card_department.blade.php
@@ -16,9 +16,9 @@
                 <div class="parent">
                     <img class="image4" src="{{ public_path('assets/images/brand/group.png') }}" width="204">
                     @if (file_exists (public_path ('/storage/app/public/images/'.$value->nik.' '.$value->employee_name.'.png')))
-                    <img class="image5" src="http://10.10.5.111/hris/public/storage/app/public/images/{{ $poto_profil }}" width="130"/>
+                    <img class="image5" src="http://10.10.5.111/hris/public/storage/app/public/images/{{ $poto_profil }}" width="150"/>
                     @else
-                    <img class="image5" src="http://10.10.5.111/hris/public/storage/app/public/images/{{ $poto_profil }}" width="130"/>
+                    <img class="image5" src="http://10.10.5.111/hris/public/storage/app/public/images/{{ $poto_profil }}" width="150"/>
                     @endif
                     <img class="image6" src="{{ public_path('assets/images/brand/foreground-test.png') }}" width="203">
                     <h6 class="text3">{{$value->nik}}</h6>
@@ -61,13 +61,13 @@
                     $poto_profil = $value->nik . ' ' . $value->employee_name . '.png';
                     $poto_profil_jpg = $value->nik . ' ' . $value->employee_name . '.jpg';
                 @endphp
-                <img class="image1" src="{{ public_path('assets/images/brand/group.png') }}" width="204">
+                <img class="image1" src="{{ public_path('assets/images/brand/group.png') }}" width="214">
                 @if (file_exists (public_path ('/storage/app/public/images/'.$value->nik.' '.$value->employee_name.'.png')))
-                <img class="image2" src="http://10.10.5.111/hris/public/storage/app/public/images/{{ $poto_profil }}" width="130"/>
+                <img class="image2" src="http://10.10.5.111/hris/public/storage/app/public/images/{{ $poto_profil }}" width="150"/>
                 @else
-                <img class="image2" src="http://10.10.5.111/hris/public/storage/app/public/images/{{ $poto_profil }}" width="130"/>
+                <img class="image2" src="http://10.10.5.111/hris/public/storage/app/public/images/{{ $poto_profil }}" width="150"/>
                 @endif
-                <img class="image3" src="{{ public_path('assets/images/brand/foreground-test.png') }}" width="203">
+                <img class="image3" src="{{ public_path('assets/images/brand/foreground-test.png') }}" width="214">
                     <h6 class="text1">{{$value->nik}}</h6>
                     <div class="row text2">
                         <div class="col" >
@@ -89,7 +89,7 @@
                             {{$value->status_jabatan}} {{$value->department_name}}
                         </div>
                         <div class="col">
-                            <img src="data:image/png;base64,{{\DNS2D::getBarcodePNG(''.$value->enroll_id, 'QRCODE',)}}" alt="barcode" style="width: 66px; height: 66px; margin:2px; margin-top:5px; margin-right:15px; padding:2px;background-color:white" />
+                            <img src="data:image/png;base64,{{\DNS2D::getBarcodePNG(''.$value->enroll_id, 'QRCODE',)}}" alt="barcode" style="width: 88px; height: 88px; margin:2px; margin-top:5px; margin-right:15px; padding:2px;background-color:white" />
                         </div>
                     </div>
             </div>
@@ -100,7 +100,6 @@
     </tr>
 </table>
 <style>
-    @page { margin-left: 2px; }
     .parent {
         position: relative;
         top: 0;
@@ -108,23 +107,22 @@
     }
     .image1 {
         position: relative;
-        top: 0;
-        left: 5px;
-    }
-    .image1 {
-        position: relative;
-        top: 0;
+        top: 80;
         left: 5;
+        width: 300px;
     }
+   
     .image2 {
         position: absolute;
-        bottom: 70px;
-        left: 8px;
+        bottom: -10px;
+        left: 4px;
+        width: 200px;
     }
     .image3 {
         position: absolute;
-        top: 421px;
+        bottom: -80px;
         left: 5;
+        width: 300px;
     }
     .garis_hitam {
         position: absolute;
@@ -133,20 +131,20 @@
     }
     .text1 {
         position: absolute;
-        top: 485px;
-        left: 152px;
-        font-weight: 600;
+        top: 800px;
+        right: 3px;
+        font-weight: bold;
         font-family:Arial, Helvetica, sans-serif;
-        font-size: 7.5px;
+        font-size: 8pt;
         letter-spacing: 1px;
     }
     .text2 {
         position: absolute;
-        width: 100px;
+        width: 150px;
         text-align: right;
-        top: 519px;
-        left: 100px;
-        font-size: 7.5px;
+        top: 850px;
+        right: 7px;
+        font-size: 11px;
         font-family:Arial, Helvetica, sans-serif;
         font-weight:600;
         color: white;
@@ -156,32 +154,35 @@
         position: relative;
         top: 80;
         left: 5;
+        width: 250px;
     }
     .image5 {
         position: absolute;
         bottom: -14px;
         left: 5px;
+        width: 200px;
     }
     .image6 {
         position: absolute;
-        top: 505px;
+        bottom: -80px;
         left: 5px;
+        width: 250px;
     }
     .text3 {
         position: absolute;
-        top: 570px;
-        left: 152px;
-        font-weight: 600;
+        top: 680px;
+        right: 3px;
+        font-weight: bold;
         font-family:Arial, Helvetica, sans-serif;
-        font-size: 7.5px;
+        font-size: 7pt;
         letter-spacing: 1px;
     }
     .text4 {
         position: absolute;
         width: 100px;
         text-align: right;
-        top: 604px;
-        left: 100px;
+        top: 725px;
+        right: 7px;
         font-size: 7.5px;
         font-family:Arial, Helvetica, sans-serif;
         font-weight:600;
