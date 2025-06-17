c4048af52140838c263a384d7f1a6265120712c6
fadli rafidan <rafidfid28@gmail.com>
2025-06-17 15:17:38 +0700

    fix: Clear unnecessary fields in export PDF for employee details

--- resources/views/hris/permintaan_tenaga_kerja/export_permintaan_tenaga_kerja_pdf.blade.php
+++ resources/views/hris/permintaan_tenaga_kerja/export_permintaan_tenaga_kerja_pdf.blade.php
@@ -777,10 +777,10 @@
             </tr>
             <tr>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;text-align:justify;vertical-align:top;border:1px solid black;text-align:center; text-transform: uppercase;">{{$value->employee_name}}</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;text-align:justify;vertical-align:top;border:1px solid black;text-align:center;">Chief/Dept. Head/Manager</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;text-align:justify;vertical-align:top;border:1px solid black;text-align:center;">HR-GA</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;text-align:justify;vertical-align:top;border:1px solid black;text-align:center;">General Mgr Produksi</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;text-align:justify;vertical-align:top;border:1px solid black;text-align:center;">General Mgr Factory</td>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;text-align:justify;vertical-align:top;border:1px solid black;text-align:center;"></td>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;text-align:justify;vertical-align:top;border:1px solid black;text-align:center;"></td>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;text-align:justify;vertical-align:top;border:1px solid black;text-align:center;"></td>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;text-align:justify;vertical-align:top;border:1px solid black;text-align:center;"></td>
             </tr>
         </thead>
 </table>
