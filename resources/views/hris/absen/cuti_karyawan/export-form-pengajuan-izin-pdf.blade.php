3266be4b6ec5f476297f90f31b1bfeb903ae2369
fadli rafidan <rafidfid28@gmail.com>
2025-06-19 16:06:43 +0700

    fix: Update PDF export form to include revision details and improve layout for employee information

--- resources/views/hris/absen/cuti_karyawan/export-form-pengajuan-izin-pdf.blade.php
+++ resources/views/hris/absen/cuti_karyawan/export-form-pengajuan-izin-pdf.blade.php
@@ -36,19 +36,19 @@
             </tr>
             <tr>
                 <td colspan="2" class="border-left" style="border: 1px solid; font-size: 9pt;">Revisi</td>
-                <td colspan="3" class="border-right" style="border: 1px solid; font-size: 9pt;">: -</td>
+                <td colspan="3" class="border-right" style="border: 1px solid; font-size: 9pt;">: Rev 1</td>
             </tr>
             <tr>
                 <td colspan="2" class="border-left" style="border: 1px solid; font-size: 9pt;">Tanggal Revisi</td>
-                <td colspan="3" class="border-right" style="border: 1px solid; font-size: 9pt;">: </td>
+                <td colspan="3" class="border-right" style="border: 1px solid; font-size: 9pt;">: 05 Maret 2024</td>
             </tr>
             <tr>
                 <td colspan="2" class="border-left" style="border: 1px solid;border-bottom: 1px solid; font-size: 9pt;">Tanggal Efektif</td>
-                <td colspan="3" class="border-right" style="border: 1px solid; border-bottom: 1px solid; font-size: 9pt;">: </td>
+                <td colspan="3" class="border-right" style="border: 1px solid; border-bottom: 1px solid; font-size: 9pt;">: 05 Maret 2024</td>
             </tr>
         </thead>
     </table>
-    <table width="100%" style="border:1px solid black; border-top:0px;">
+    <table width="100%" style="border-left:1px solid black; border-top:0px; border-right:1px solid black;">
         <thead>
             <tr>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; height:10px;" colspan="6"></td>
@@ -56,7 +56,12 @@
             <tr>
                 <td style="padding-left: 10px !important; font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top;" width="17%">Nama</td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top;" width="1%">:</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; text-transform: uppercase;" width="42%">{{ $data->employee_name }}</td>
+            <td style="font-family: Arial, Helvetica, sans-serif; font-size: 8pt; vertical-align: top; text-transform: uppercase; line-height: normal;" width="100%">
+                {{ $data->employee_name }}
+                <div style="border-bottom: 1px solid #000; width: 100%; margin-top: 0px;">
+                    <span style="display: block; height: 1px;"></span>
+                </div>
+            </td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top;" width="16%"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top;" width="1%"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top;"></td>
@@ -67,7 +72,11 @@
             <tr>
                 <td style="padding-left: 10px !important; font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top">NIP</td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top">:</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; text-transform: uppercase;">{{ $data->nik }}</td>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; text-transform: uppercase; line-height: normal;">{{ $data->nik }}
+                    <div style="border-bottom: 1px solid #000; width: 100%; margin-top: 0px;">
+                    <span style="display: block; height: 1px;"></span>
+                </div>
+                </td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
@@ -78,7 +87,11 @@
             <tr>
                 <td style="padding-left: 10px !important; font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top;width:150px">Bagian</td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top">:</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; text-transform: uppercase;">{{ $data->sub_dept_name }}<</td>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; text-transform: uppercase; line-height: normal;">{{ $data->sub_dept_name }}
+                    <div style="border-bottom: 1px solid #000; width: 100%; margin-top: 0px;">
+                    <span style="display: block; height: 1px;"></span>
+                </div>
+                </td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
@@ -89,7 +102,10 @@
             <tr>
                 <td style="padding-left: 10px !important; font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top">Department</td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top">:</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; text-transform: uppercase;">{{ $data->department_name }}<</td>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; text-transform: uppercase; line-height: normal;">{{ $data->department_name }}
+                    <div style="border-bottom: 1px solid #000; width: 100%; margin-top: 0px;">
+                    <span style="display: block; height: 1px;"></span>
+                </td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
@@ -122,13 +138,61 @@
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
             </tr>
+
             <tr>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; height:10px;" colspan="6"></td>
             </tr>
-            <tr>
-                <td style="padding-left: 10px !important; font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top">Keterangan</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top">:</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; text-transform: uppercase;"> {{ $data->absen_alasan }}</td>
+        </thead>
+    </table>
+  @php
+    function checkboxRow($label, $kode, $data) {
+        $realKode = $data->kode_absen_ijin;
+        $isChecked = $realKode === $kode;
+
+        $icon = $isChecked
+            ? '<div style="width:15px; height:15px; border:1px solid #000; text-align:center;"><img src="' . public_path('assets/images/brand/check-mark-icon-5374.jpg') . '" style="width:12px; height:12px;" /></div>'
+            : '<div style="width:15px; height:15px; border:1px solid #000;"></div>';
+
+        return '
+            <table style="margin-bottom: 3px;">
+                <tr>
+                    <td style="vertical-align: middle !important; padding: 0; padding-right: 5px;">
+                        ' . $icon . '
+                    </td>
+                    <td style="vertical-align: middle !important; font-size: 8pt; font-family: Arial, sans-serif;">
+                        ' . $label . '
+                    </td>
+                </tr>
+            </table>
+        ';
+    }
+@endphp
+
+    <table style="width: 100%; border-left: 1px solid #000; border-right: 1px solid #000; border-collapse: collapse; font-family: Arial, sans-serif; font-size: 8pt;">
+        <tr>
+        <td style="width: 50%; vertical-align: top; padding-left: 20px;">
+            {!! checkboxRow('Datang terlambat', 'DT', $data) !!}
+            {!! checkboxRow('Pulang lebih awal', 'PC', $data) !!}
+            {!! checkboxRow('Ijin keluar perusahaan sementara', 'IK', $data) !!}
+        </td>
+        <td style="width: 50%; vertical-align: top;">
+             {!! checkboxRow('Dinas Luar', 'DL', $data) !!}
+            {!! checkboxRow('Tidak masuk kerja', 'TMK', $data) !!}
+        </td>
+        </tr>
+    </table>
+
+
+
+    <table width="100%" style="border-left:1px solid black; border-top:0px; border-right:1px solid black; margin: none; padding:none;">
+        <thead>
+             <tr>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; height:10px;" colspan="6"></td>
+            </tr>
+             <tr>
+                <td style="padding-left: 10px !important; font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; width:21.5%">Keterangan</td>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; width: 1%">:</td>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; text-transform: uppercase; border: 1px solid #000; height:30px; padding: 5px"> {{ $data->absen_alasan }}</td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
@@ -136,11 +200,18 @@
             <tr>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; height:10px;" colspan="6"></td>
             </tr>
+            @php
+                $tanggal = \Carbon\Carbon::parse($data->tanggal_mulai_ijin ?? $data->tanggal_perizinan);
+                $waktu = $data->time_mulai_ijin;
+
+                $tanggal_sampai = \Carbon\Carbon::parse($data->tanggal_akhir_ijin ?? $data->tanggal_perizinan);
+                $waktu_sampai = $data->time_akhir_ijin;
+            @endphp
             <tr>
                 <td style="padding-left: 10px !important; font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top">Mulai</td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top">:</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; text-transform: uppercase;"> {{ \Carbon\Carbon::parse($data->tanggal_mulai_ijin ? $data->tanggal_mulai_ijin : $data->tanggal_perizinan)->translatedFormat('d F Y') }} {{ $data->time_mulai_ijin  }}</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; width: 50%; ">Hari <b>{{ $tanggal->translatedFormat('l') }}</b> Tanggal <b>{{ $tanggal->translatedFormat('d') }}</b> Bulan <b>{{ $tanggal->translatedFormat('F') }}</b> Tahun <b>{{ $tanggal->translatedFormat('Y') }}</b> Jam <b>{{ $waktu }}</b></td>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top;"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
             </tr>
@@ -150,7 +221,7 @@
             <tr>
                 <td style="padding-left: 10px !important; font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top">Sampai</td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top">:</td>
-                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; text-transform: uppercase;">{{ \Carbon\Carbon::parse($data->tanggal_akhir_ijin ? $data->tanggal_akhir_ijin : $data->tanggal_perizinan)->translatedFormat('d F Y') }} {{ $data->time_akhir_ijin }}</td>
+                <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top;  width: 50%;">Hari <b>{{ $tanggal_sampai->translatedFormat('l') }}</b> Tanggal <b>{{ $tanggal_sampai->translatedFormat('d') }}</b> Bulan <b>{{ $tanggal_sampai->translatedFormat('F') }}</b> Tahun <b>{{ $tanggal_sampai->translatedFormat('Y') }}</b> Jam <b>{{ $waktu_sampai }}</td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top"></td>
@@ -170,9 +241,9 @@
             <tr>
                 <td style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;vertical-align:top; height:10px;" colspan="6"></td>
             </tr>
-        </thead>
+            </thead>
     </table>
-    <table width="100%">
+    <table width="100%" style="border-top:1px solid black;">
         <thead>
             <tr>
                 <td width="21%" style="font-family:Arial, Helvetica, sans-serif;font-size:8pt;text-align:justify;vertical-align:top;border:1px solid black;text-align:center; border-top:0px;" >Dibuat</td>
