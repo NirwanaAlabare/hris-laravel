93f6f62da3a7ecbb1eecee72efd0f50bff595469
fadli rafidan <rafidfid28@gmail.com>
2025-07-11 11:07:19 +0700

    e

--- app/Http/Controllers/Hris/GAController.php
+++ app/Http/Controllers/Hris/GAController.php
@@ -325,7 +325,7 @@ class GAController extends AdminBaseController
     }
     public function ajax_getallemployeeatribut()
     {
-        $query =  EmployeeAtribut::selectRaw('enroll_id, nik, employee_name, concat(enroll_id, " - ", nik, " - ", employee_name) select_employee')->groupby('enroll_id')->orderby('employee_name', 'asc')->get();
+        $query =  EmployeeAtribut::selectRaw('enroll_id, nik, employee_name, concat(enroll_id, " - ", nik, " - ", employee_name) select_employee')->where('status_aktif', 'AKTIF')->groupby('enroll_id')->orderby('employee_name', 'asc')->get();
         return $query;
 
     }
