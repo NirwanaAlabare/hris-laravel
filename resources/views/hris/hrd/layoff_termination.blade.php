9ca6e43a30e354a6a0ece596c8074191216b9256
fadli rafidan <rafidfid28@gmail.com>
2025-07-18 15:40:41 +0700

    buat fitur hapus tanggal

--- resources/views/hris/hrd/layoff_termination.blade.php
+++ resources/views/hris/hrd/layoff_termination.blade.php
@@ -94,11 +94,17 @@
                                    <div class="col-1 pt-1">
                                     <label class="form-label" style="font-weight: bold; color:rgb(99, 99, 132);font-size:12pt"> Periode</label>
                                 </div>
-                                    <div class="col-md-3">
-                                        <div class="form-group m-0">
-                                            <input type="hidden" id="daterange1" name="daterange1">
-                                            <a class="nav-link card-title py-2 pl-3" style="border: 1px solid #d8d4dc" id="daterange-btn1" data-toggle="tooltip" title="" data-placement="bottom" data-original-title="Klik di sini untuk pilih tanggal kehadiran"></a>
-                                        </div>
+                                <div class="col-md-3">
+                                    <div class="d-flex">
+                                            <div class="form-group m-0">
+                                                <input type="hidden" id="daterange1" name="daterange1">
+                                                <a class="nav-link card-title py-2 pl-3" style="border: 1px solid #d8d4dc" id="daterange-btn1" data-toggle="tooltip" title="" data-placement="bottom" data-original-title="Klik di sini untuk pilih tanggal kehadiran"></a>
+                                            </div>
+                                            <button id="clear-daterange" style="height: 36px; justify-content: center; align-items: center;"
+                                                class="btn btn-outline-danger ml-1" data-toggle="tooltip" title="Hapus Periode">
+                                                <i class="fa fa-times m-0 p-0"></i>
+                                            </button>
+                                    </div>
                                 </div>
                                 <div class="col-2">
                                     <button class="btn btn-success btn-app" id="btn_export_excel_layoff_currday" onclick="export_excel()">
@@ -208,44 +214,99 @@
 </style>
 <script type="text/javascript">
 
+    // $(document).ready(function() {
+    //     const start = moment().subtract(29, 'days');
+    //     const end = moment();
+
+    //     // Set nilai awal
+    //     updateRange(start, end);
+    //     function updateRange(start, end) {
+    //         $('#daterange-btn1').html(
+    //             '<span><i class="fa fa-calendar"></i> ' +
+    //             start.format("D MMM YYYY").toUpperCase() +
+    //             ' s/d ' +
+    //             end.format("D MMM YYYY").toUpperCase() +
+    //             '</span><i class="fa fa-angle-down ml-1"></i>'
+    //         );
+
+    //         const daterange1 = start.format("YYYY-MM-DD") + " s/d " + end.format("YYYY-MM-DD");
+    //         $('#daterange1').val(daterange1);
+    //     }
+
+    //     // Inisialisasi dan isi daterange1 terlebih dahulu
+    //     $('#daterange-btn1').daterangepicker({
+    //         ranges: {
+    //             'Hari ini': [moment(), moment()],
+    //             'Kemarin': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+    //             '7 Hari Kemarin': [moment().subtract(6, 'days'), moment()],
+    //             '30 Hari Kemarin': [moment().subtract(29, 'days'), moment()],
+    //             'Bulan Sekarang': [moment().startOf('month'), moment().endOf('month')],
+    //             'Bulan Kemarin': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    //         },
+    //         startDate: start,
+    //         endDate: end,
+    //         maxDate: moment()
+    //     }, function (start, end) {
+    //         updateRange(start, end);
+    //         datatable.ajax.reload(); // reload setelah user pilih tanggal
+    //     });
+
+    // });
+
+    // Tombol clear
+$('#clear-daterange').on('click', function () {
+    $('#daterange1').val('');
+    $('#daterange-btn1').html(
+        '<span><i class="fa fa-calendar"></i> Pilih Tanggal</span><i class="fa fa-angle-down ml-1"></i>'
+    );
+    datatable.ajax.reload(); // reload data tanpa filter tanggal
+});
+
+
     $(document).ready(function() {
-        const start = moment().subtract(29, 'days');
-        const end = moment();
+    const start = moment().subtract(29, 'days');
+    const end = moment();
 
-        // Set nilai awal
+    // Hapus updateRange() di awal, biarkan kosong
+    $('#daterange-btn1').html(
+        '<span><i class="fa fa-calendar mr-2"></i>   Pilih Range Tanggal   </span><i class="fa fa-angle-down ml-2"></i>'
+    );
+
+    $('#daterange1').val(''); // Pastikan input filter kosong di awal
+
+    // Fungsi updateRange tetap ada untuk dipanggil setelah user memilih tanggal
+    function updateRange(start, end) {
+        $('#daterange-btn1').html(
+            '<span><i class="fa fa-calendar"></i> ' +
+            start.format("D MMM YYYY").toUpperCase() +
+            ' s/d ' +
+            end.format("D MMM YYYY").toUpperCase() +
+            '</span><i class="fa fa-angle-down ml-1"></i>'
+        );
+
+        const daterange1 = start.format("YYYY-MM-DD") + " s/d " + end.format("YYYY-MM-DD");
+        $('#daterange1').val(daterange1);
+    }
+
+    $('#daterange-btn1').daterangepicker({
+        ranges: {
+            'Hari ini': [moment(), moment()],
+            'Kemarin': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            '7 Hari Kemarin': [moment().subtract(6, 'days'), moment()],
+            '30 Hari Kemarin': [moment().subtract(29, 'days'), moment()],
+            'Bulan Sekarang': [moment().startOf('month'), moment().endOf('month')],
+            'Bulan Kemarin': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        startDate: start,
+        endDate: end,
+        maxDate: moment()
+    }, function (start, end) {
         updateRange(start, end);
-        function updateRange(start, end) {
-            $('#daterange-btn1').html(
-                '<span><i class="fa fa-calendar"></i> ' +
-                start.format("D MMM YYYY").toUpperCase() +
-                ' s/d ' +
-                end.format("D MMM YYYY").toUpperCase() +
-                '</span><i class="fa fa-angle-down ml-1"></i>'
-            );
-
-            const daterange1 = start.format("YYYY-MM-DD") + " s/d " + end.format("YYYY-MM-DD");
-            $('#daterange1').val(daterange1);
-        }
-
-        // Inisialisasi dan isi daterange1 terlebih dahulu
-        $('#daterange-btn1').daterangepicker({
-            ranges: {
-                'Hari ini': [moment(), moment()],
-                'Kemarin': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                '7 Hari Kemarin': [moment().subtract(6, 'days'), moment()],
-                '30 Hari Kemarin': [moment().subtract(29, 'days'), moment()],
-                'Bulan Sekarang': [moment().startOf('month'), moment().endOf('month')],
-                'Bulan Kemarin': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-            },
-            startDate: start,
-            endDate: end,
-            maxDate: moment()
-        }, function (start, end) {
-            updateRange(start, end);
-            datatable.ajax.reload(); // reload setelah user pilih tanggal
-        });
-
+        datatable.ajax.reload(); // reload setelah user pilih tanggal
     });
+
+});
+
 </script>
 <script type="text/javascript">
 
