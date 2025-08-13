e307904cd38db5bed7b9c6cd14aaf281c841866c
fadli rafidan <rafidfid28@gmail.com>
2025-08-13 09:55:56 +0700

    ubah logic extend contract

--- resources/views/hris/hrd/kontrak_kerja.blade.php
+++ resources/views/hris/hrd/kontrak_kerja.blade.php
@@ -2888,11 +2888,15 @@ function updateRange(start, end) {
 
                 var last_date=new Date(res[res.length-1]['contract_end']);
 
+                last_date.setDate(last_date.getDate()+1);
                 var dayOfWeek = last_date.getDay();
+
                 if (dayOfWeek === 5) { // Jumat
-                    last_date.setDate(last_date.getDate());
-                }else{
-                    last_date.setDate(last_date.getDate()+1);
+                    last_date.setDate(last_date.getDate()); // mundur ke Jumat
+                } else if (dayOfWeek === 6) { // Sabtu
+                    last_date.setDate(last_date.getDate() - 1); // mundur ke Jumat
+                } else if (dayOfWeek === 0) { // Minggu
+                    last_date.setDate(last_date.getDate() - 2); // mundur ke Jumat
                 }
 
                 var dd = String(last_date.getDate()).padStart(2, '0');
