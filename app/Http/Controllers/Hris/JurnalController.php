645100e5569fa88423894a7d7864140f14736c5a
ranusu <ramdhan_nugraha99@gmail.com>
2025-10-31 11:27:51 +0700

    add controller

--- app/Http/Controllers/Hris/JurnalController.php
+++ app/Http/Controllers/Hris/JurnalController.php
@@ -14,6 +14,9 @@ use Datatables;
 use Maatwebsite\Excel\Facades\Excel;
 use Carbon\Carbon;
 use App\Exports\JurnalExport;
+use App\Models\DataKoreksiPotongan;
+use App\Models\DataKoreksiUpah;
+use App\Models\DepartmentAll;
 
 /**
  * Class RekapPerhitunganPayrollController
@@ -43,22 +46,138 @@ class JurnalController extends AdminBaseController
     public function ajax_getperiodepayroll()
     {
         $query =  Jurnal::selectRaw('periode_payroll')
-                        ->groupby('periode_payroll')
-                        ->orderby('periode_payroll', 'desc')
-                        ->get();
+            ->groupby('periode_payroll')
+            ->orderby('periode_payroll', 'desc')
+            ->get();
         return $query;
-
     }
     public function ajax_exportexcel(Request $request)
     {
-        $data=Jurnal::where('periode_payroll',$request->periode_payroll)->get();
+        $data = Jurnal::where('periode_payroll', $request->periode_payroll)->get();
 
-        return Excel::download(new JurnalExport($data),'Data_jurnal'.time().'.xlsx');
+        return Excel::download(new JurnalExport($data), 'Data_jurnal' . time() . '.xlsx');
     }
 
     public function update_jurnal(Request $request)
     {
-        dd($request->periode_payroll);
+        //rekap jurnal
+        $departement = DepartmentAll::where('site_nirwana_id', 'NAG')->get();
+        $data_potongan = $this->potongan($periode_payroll);
+        $data_koreksi = $this->koreksi($periode_payroll);
+        foreach ($departement as $key => $value) {
+            $potongan_bpjs_tk = $data_potongan->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_potongan', '1')->sum('jumlah_rp_potongan');
+            $potongan_bpjs_ks = $data_potongan->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_potongan', '2')->sum('jumlah_rp_potongan');
+            $potongan_bazzar = $data_potongan->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_potongan', '3')->sum('jumlah_rp_potongan');
+            $potongan_kasbon = $data_potongan->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_potongan', '6')->sum('jumlah_rp_potongan');
+            $potongan_lain = $data_potongan->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_potongan', '5')->sum('jumlah_rp_potongan');
+            $koreksi_upah = $data_koreksi->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_koreksi', '1')->sum('jumlah_rp_potongan');
+            $koreksi_insentif = $data_koreksi->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_koreksi', '2')->sum('jumlah_rp_potongan');
+            $payroll = RekapPerhitunganPayroll::where('periode_tahun_payroll', $year)->where('periode_bulan_payroll', $month)->where('periode_umk', null)
+                ->where('kategori_karyawan', 'NON STAFF')->where('sub_dept_id', $value->sub_dept_id)
+                ->where('total_kehadiran_net', '>', 0)->get();
+
+
+            $rp_cuti_tahuna = 0;
+            $potongan_kehadiran_rupiah = $payroll->sum('potongan_kehadiran_rupiah');
+            $rp_pot_jam = $payroll->sum('potongan_iks_rupiah') + $payroll->sum('potongan_dtpc_rupiah');
+            $iuran_serikat_rupiah = $payroll->sum('iuran_serikat_rupiah');
+            $iuran_koperasi = $payroll->sum('iuran_koperasi');
+
+            $gaji_umk = $payroll->sum('upah_per_bulan');
+            $pembulatan = $payroll->sum('pembulatan');
+            $gaji = $gaji_umk + $pembulatan + $koreksi_upah;
+            $insentif_jabatan = $payroll->sum('insentif_jabatan');
+            $premi_karyawan = $payroll->sum('premi_karyawan');
+            $tunjangan_karyawan_rupiah = ($payroll->sum('tunjangan_karyawan_rupiah') + $koreksi_insentif + $insentif_jabatan + $premi_karyawan);
+            $total_lembur_rupiah = $payroll->sum('total_lembur_rupiah');
+            $bonus = 0;
+            $piutang_karyawan = $potongan_kasbon;
+            $piutang_bazzar = $potongan_bazzar;
+            $bpjs_tk = $payroll->sum('total_bpjs_tk');
+            $bpjs_ks = $payroll->sum('total_bpjs_ks');
+            $potongan = $potongan_lain + $rp_cuti_tahuna + $potongan_kehadiran_rupiah + $rp_pot_jam + $iuran_serikat_rupiah + $iuran_koperasi;
+            // $gaji_note=($gaji+$tunjangan_karyawan_rupiah+$total_lembur_rupiah+$bonus)-
+            //             ($piutang_karyawan+$piutang_bazzar+$bpjs_tk+$bpjs_ks+$potongan);
+            $gaji_neto = $payroll->sum('total_upah_thp_rupiah') + $payroll->sum('pembulatan');
+
+            $data = [
+                'kode_bagian' => $value->sub_dept_id,
+                'nama_bagian' => $value->sub_dept_name,
+                'gaji' => $gaji,
+                'tunjangan_karyawan_rupiah' => $tunjangan_karyawan_rupiah,
+                'total_lembur_rupiah' => $total_lembur_rupiah,
+                'bonus' => $bonus,
+                'piutang_karyawan' => $piutang_karyawan,
+                'piutang_bazzar' => $piutang_bazzar,
+                'bpjs_tk' => $bpjs_tk,
+                'bpjs_ks' => $bpjs_ks,
+                'potongan' => $potongan,
+                'gaji_neto' => $gaji_neto,
+                'jumlah_karyawn' => $payroll->count(),
+                'periode_payroll' => $periode_payroll,
+            ];
+            $count = Jurnal::where('kode_bagian', $value->sub_dept_id)->where('periode_payroll', $periode_payroll)->count();
+
+            if ($count) {
+                Jurnal::where('kode_bagian', $value->sub_dept_id)->where('periode_payroll', $periode_payroll)->update($data);
+            } else {
+                Jurnal::create($data);
+            }
+        }
     }
 
+    public function potongan($periode_payroll)
+    {
+        $DataKoreksiPotongan = DataKoreksiPotongan::where('periode_tanggal_koreksi', $periode_payroll)->get();
+        $data = [];
+        foreach ($DataKoreksiPotongan as $key => $value) {
+            $data[] = [
+                'enroll_id' => $value->enroll_id,
+                'nik' => $value->nik,
+                'employee_name' => $value->employee_name,
+                'site_nirwana_id' => $value->site_nirwana_id,
+                'site_nirwana_name' => $value->site_nirwana_name,
+                'department_id' => $value->department_id,
+                'department_name' => $value->department_name,
+                'sub_dept_id' => $value->sub_dept_id,
+                'sub_dept_name' => $value->sub_dept_name,
+                'jumlah_rp_potongan' => $value->jumlah_rp_potongan,
+                'periode_tanggal_koreksi' => $value->periode_tanggal_koreksi,
+                'jenis_potongan' => $value->jenis_potongan,
+                'keterangan' => $value->keterangan,
+                'status_jabatan' => $value->atribut->status_staff,
+            ];
+        }
+
+        $koreksi_potongan = collect($data);
+
+        return  $koreksi_potongan;
+    }
+    public function koreksi($periode_payroll)
+    {
+        $DataKoreksiUpah = DataKoreksiUpah::where('periode_tanggal_koreksi', $periode_payroll)->get();
+        $data = [];
+        foreach ($DataKoreksiUpah as $key => $value) {
+            $data[] = [
+                'enroll_id' => $value->enroll_id,
+                'nik' => $value->nik,
+                'employee_name' => $value->employee_name,
+                'site_nirwana_id' => $value->site_nirwana_id,
+                'site_nirwana_name' => $value->site_nirwana_name,
+                'department_id' => $value->department_id,
+                'department_name' => $value->department_name,
+                'sub_dept_id' => $value->sub_dept_id,
+                'sub_dept_name' => $value->sub_dept_name,
+                'jumlah_rp_potongan' => $value->jumlah_rp_potongan,
+                'periode_tanggal_koreksi' => $value->periode_tanggal_koreksi,
+                'jenis_koreksi' => $value->jenis_koreksi,
+                'keterangan' => $value->keterangan,
+                'status_jabatan' => $value->atribut->status_staff,
+            ];
+        }
+
+        $data_koreksi = collect($data);
+
+        return $data_koreksi;
+    }
 }
