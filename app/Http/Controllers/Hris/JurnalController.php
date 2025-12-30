5f7fc35b175a63e91d4a89fce4174127f13d4e2e
zidan <muhammadzidan3110@gmail.com>
2025-12-30 16:13:38 +0700

    selisih

--- app/Http/Controllers/Hris/JurnalController.php
+++ app/Http/Controllers/Hris/JurnalController.php
@@ -67,6 +67,121 @@ class JurnalController extends AdminBaseController
         return Excel::download(new JurnalExport($data), 'Data_jurnal' . time() . '.xlsx');
     }
 
+//     public function update_jurnal(Request $request)
+//     {
+//         $periode_payroll = $request->periode_payroll;
+
+//         // Ambil 4 karakter pertama dari kiri (tahun)
+//         $year = substr($periode_payroll, 0, 4);
+
+//         // Ambil 2 karakter terakhir dari kanan (bulan)
+//         $month = substr($periode_payroll, -2);
+
+
+//         //rekap jurnal
+//         $departement = DepartmentAll::get();
+//         $data_potongan = $this->potongan($periode_payroll);
+//         $data_koreksi = $this->koreksi($periode_payroll);
+//         foreach ($departement as $key => $value) {
+//             $potongan_bpjs_tk = $data_potongan->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_potongan', '1')->sum('jumlah_rp_potongan');
+//             $potongan_bpjs_ks = $data_potongan->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_potongan', '2')->sum('jumlah_rp_potongan');
+//             $potongan_bazzar = $data_potongan->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_potongan', '3')->sum('jumlah_rp_potongan');
+//             $potongan_kasbon = $data_potongan->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_potongan', '6')->sum('jumlah_rp_potongan');
+//             $potongan_lain = $data_potongan->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_potongan', '5')->sum('jumlah_rp_potongan');
+//             // $koreksi_insentif = $data_koreksi->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_koreksi', '2')->sum('jumlah_rp_potongan');
+//             $payroll = RekapPerhitunganPayroll::where('periode_tahun_payroll', $year)->where('periode_bulan_payroll', $month)->where('periode_umk', null)
+//                 ->where('kategori_karyawan', 'NON STAFF')->where('sub_dept_id', $value->sub_dept_id)
+//                 ->get();
+
+
+//             $rp_cuti_tahuna = 0;
+//             $potongan_kehadiran_rupiah = $payroll->sum('potongan_kehadiran_rupiah');
+//             $rp_pot_jam = $payroll->sum('potongan_iks_rupiah') + $payroll->sum('potongan_dtpc_rupiah');
+//             $iuran_serikat_rupiah = $payroll->sum('iuran_serikat_rupiah');
+//             $iuran_koperasi = $payroll->sum('iuran_koperasi');
+
+//             $gaji_umk = $payroll->sum('upah_per_bulan');
+//             $koreksi_upah = $payroll->sum('koreksi_upah');
+//             $potongan_upah = $payroll->sum('potongan_upah');
+
+//             $total_gaji = 0;
+//             $total_tunjangan_karyawan = 0;
+
+//             foreach ($payroll as $p) {
+//                 $nama_bank = $p->nama_bank ?? '-';
+//                 $tunai = strtoupper($nama_bank) === 'TUNAI';
+
+//                 $nilai_bersih = $p->upah_neto_rupiah - $p->jumlah_potongan_rupiah;
+// // 924123562
+
+//                 if ($tunai) {
+//                     // Pembulatan ke atas kelipatan 500
+//                     $total_upah_thp_rupiah_pembulatan = ceil($nilai_bersih / 500) * 500;
+//                 } else {
+//                     // Pembulatan ke atas kelipatan 100
+//                     $total_upah_thp_rupiah_pembulatan = ceil($nilai_bersih / 100) * 100;
+//                 }
+//                 $pembulatan = $total_upah_thp_rupiah_pembulatan - $nilai_bersih;
+
+//                 $total_gaji += ($p->upah_per_bulan + $pembulatan + $p->koreksi_upah) - $p->potongan_upah;
+
+//                 if ($p->total_kehadiran_net <= 0 && $p->koreksi_upah_rupiah == 0 && $p->total_lembur_rupiah == 0 && ($p->total_bpjs_tk != 0 || $p->total_bpjs_ks != 0)) {
+//                     continue;
+//                 }
+
+//                 if ($p->total_kehadiran_net == 0) {
+//                     continue;
+//                 }
+//                 // Kalau lolos kondisi di atas, baru tambahkan
+//                 $total_tunjangan_karyawan += $p->tunjangan_karyawan_rupiah;
+//             }
+//             $gaji = $total_gaji;
+//             $insentif_jabatan = $payroll->sum('insentif_jabatan');
+//             $premi_karyawan = $payroll->sum('premi_karyawan');
+//             $koreksi_insentif = $payroll->sum('koreksi_insentif');
+//             $potongan_insentif = $payroll->sum('potongan_insentif');
+
+//             $potongan_piutang = $payroll->sum('potongan_piutang');
+
+
+//             $tunjangan_karyawan_rupiah = ($total_tunjangan_karyawan + $koreksi_insentif + $insentif_jabatan + $premi_karyawan) - $potongan_insentif;
+//             $total_lembur_rupiah = $payroll->sum('total_lembur_rupiah');
+//             $bonus = 0;
+//             $piutang_karyawan = $potongan_piutang;
+//             $piutang_bazzar = $potongan_bazzar;
+//             $bpjs_tk = $payroll->sum('total_bpjs_tk');
+//             $bpjs_ks = $payroll->sum('total_bpjs_ks');
+//             $potongan = $potongan_lain + $rp_cuti_tahuna + $potongan_kehadiran_rupiah + $rp_pot_jam + $iuran_serikat_rupiah + $iuran_koperasi;
+//             $gaji_note=($gaji+$tunjangan_karyawan_rupiah+$total_lembur_rupiah+$bonus)-
+//                         ($piutang_karyawan+$piutang_bazzar+$bpjs_tk+$bpjs_ks+$potongan);
+//             $gaji_neto = ($gaji + $tunjangan_karyawan_rupiah + $total_lembur_rupiah) - ($piutang_karyawan + $piutang_bazzar + $bpjs_tk + $bpjs_ks + $potongan);
+
+//             $data = [
+//                 'kode_bagian' => $value->sub_dept_id,
+//                 'nama_bagian' => $value->sub_dept_name,
+//                 'gaji' => $gaji,
+//                 'tunjangan_karyawan_rupiah' => $tunjangan_karyawan_rupiah,
+//                 'total_lembur_rupiah' => $total_lembur_rupiah,
+//                 'bonus' => $bonus,
+//                 'piutang_karyawan' => $piutang_karyawan,
+//                 'piutang_bazzar' => $piutang_bazzar,
+//                 'bpjs_tk' => $bpjs_tk,
+//                 'bpjs_ks' => $bpjs_ks,
+//                 'potongan' => $potongan,
+//                 'gaji_neto' => $gaji_neto,
+//                 'jumlah_karyawn' => $payroll->count(),
+//                 'periode_payroll' => $periode_payroll,
+//             ];
+//             $count = Jurnal::where('kode_bagian', $value->sub_dept_id)->where('periode_payroll', $periode_payroll)->count();
+
+//             if ($count) {
+//                 Jurnal::where('kode_bagian', $value->sub_dept_id)->where('periode_payroll', $periode_payroll)->update($data);
+//             } else {
+//                 Jurnal::create($data);
+//             }
+//         }
+//     }
+
     public function update_jurnal(Request $request)
     {
         $periode_payroll = $request->periode_payroll;
@@ -76,7 +191,37 @@ class JurnalController extends AdminBaseController
 
         // Ambil 2 karakter terakhir dari kanan (bulan)
         $month = substr($periode_payroll, -2);
-
+        // $gajiNetoPerDept = DB::table('rekap_perhitungan_payroll as rpp')
+        // ->join('employee_atribut as ea', 'ea.enroll_id', '=', 'rpp.enroll_id')
+        // ->selectRaw("
+        //     rpp.sub_dept_id,
+        //     SUM(
+        //         CASE
+        //             WHEN rpp.total_kehadiran_net <= 0
+        //                  AND rpp.koreksi_upah_rupiah = 0
+        //                  AND rpp.total_lembur_rupiah = 0
+        //                  AND (rpp.total_bpjs_tk <> 0 OR rpp.total_bpjs_ks <> 0)
+        //             THEN 0
+        //             ELSE
+        //                 CASE
+        //                     WHEN UPPER(rpp.nama_bank) = 'TUNAI'
+        //                     THEN CEILING((rpp.upah_neto_rupiah - rpp.jumlah_potongan_rupiah) / 500) * 500
+        //                     ELSE CEILING((rpp.upah_neto_rupiah - rpp.jumlah_potongan_rupiah) / 100) * 100
+        //                 END
+        //         END
+        //     ) AS total_gaji
+        // ")
+        // ->where('rpp.periode_tahun_payroll', $year)
+        // ->where('rpp.periode_bulan_payroll', $month)
+        // ->whereNull('rpp.periode_umk')
+        // ->where('rpp.kategori_karyawan', 'NON STAFF')
+        // ->where('rpp.total_kehadiran_net', '>', 0)
+        // ->where(function ($q) {
+        //     $q->whereNull('ea.tanggal_resign')
+        //       ->orWhere('ea.tanggal_resign', '>', '2025-11-26');
+        // })
+        // ->groupBy('rpp.sub_dept_id')
+        // ->pluck('total_gaji', 'sub_dept_id');
 
         //rekap jurnal
         $departement = DepartmentAll::get();
@@ -90,7 +235,7 @@ class JurnalController extends AdminBaseController
             $potongan_lain = $data_potongan->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_potongan', '5')->sum('jumlah_rp_potongan');
             // $koreksi_insentif = $data_koreksi->where('sub_dept_id', $value->sub_dept_id)->where('status_jabatan', 'NON STAFF')->where('jenis_koreksi', '2')->sum('jumlah_rp_potongan');
             $payroll = RekapPerhitunganPayroll::where('periode_tahun_payroll', $year)->where('periode_bulan_payroll', $month)->where('periode_umk', null)
-                ->where('kategori_karyawan', 'NON STAFF')->where('sub_dept_id', $value->sub_dept_id)
+                ->where('kategori_karyawan', 'NON STAFF')->where('sub_dept_id', $value->sub_dept_id)->where('total_kehadiran_net', '>', 0)
                 ->get();
 
 
@@ -151,9 +296,10 @@ class JurnalController extends AdminBaseController
             $bpjs_tk = $payroll->sum('total_bpjs_tk');
             $bpjs_ks = $payroll->sum('total_bpjs_ks');
             $potongan = $potongan_lain + $rp_cuti_tahuna + $potongan_kehadiran_rupiah + $rp_pot_jam + $iuran_serikat_rupiah + $iuran_koperasi;
-            $gaji_note=($gaji+$tunjangan_karyawan_rupiah+$total_lembur_rupiah+$bonus)-
-                        ($piutang_karyawan+$piutang_bazzar+$bpjs_tk+$bpjs_ks+$potongan);
+            // $gaji_note=($gaji+$tunjangan_karyawan_rupiah+$total_lembur_rupiah+$bonus)-
+            //             ($piutang_karyawan+$piutang_bazzar+$bpjs_tk+$bpjs_ks+$potongan);
             $gaji_neto = ($gaji + $tunjangan_karyawan_rupiah + $total_lembur_rupiah) - ($piutang_karyawan + $piutang_bazzar + $bpjs_tk + $bpjs_ks + $potongan);
+            // $gaji_neto = $gajiNetoPerDept[$value->sub_dept_id] ?? 0;
 
             $data = [
                 'kode_bagian' => $value->sub_dept_id,
@@ -180,6 +326,7 @@ class JurnalController extends AdminBaseController
             }
         }
     }
+
 
     public function potongan($periode_payroll)
     {
