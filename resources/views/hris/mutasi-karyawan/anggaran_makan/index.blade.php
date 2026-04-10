e8345b2a3c9d266f25f547df88bc2424e3cb2e48
zidan <muhammadzidan3110@gmail.com>
2026-04-10 17:03:08 +0700

    anggaran makan

--- resources/views/hris/mutasi-karyawan/anggaran_makan/index.blade.php
+++ resources/views/hris/mutasi-karyawan/anggaran_makan/index.blade.php
@@ -320,9 +320,9 @@
                 if(stringSelectDate<stringCurrentDate){
                     $('#btn_new').attr('disabled','disabled');
                 }else if(stringCurrentDate==stringSelectDate){
-                    if(currentdate.getHours()>16){
+                    if(currentdate.getHours()>12){
                         $('#btn_new').attr('disabled','disabled');
-                    }else if(currentdate.getHours()<16){
+                    }else if(currentdate.getHours()<13){
                         $('#btn_new').removeAttr('disabled');
                     }
                 }else{
